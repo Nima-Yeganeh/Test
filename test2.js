6a48e5df5dd72267ab2b8b624e34a751cba6e826
nima-yeganeh <yeganeh.nima.d@gmail.com>
2023-05-10 14:38:25 +0000

    done

--- test2.js
+++ test2.js
@@ -1,7 +1,7 @@
 const { exec } = require('child_process');
 
 // Run a shell command
-const command = 'bash gitupdate.sh';
+const command = 'uptime';
 exec(command, (error, stdout, stderr) => {
   if (error) {
     console.error(`Error: ${error.message}`);
