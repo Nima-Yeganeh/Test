e0689d6806ddba0c680bf25a161eb9c2591ef2c9
root <yeganeh.nima.d@gmail.com>
2023-05-10 17:08:02 +0330

    done

---
+++ test4.sh
@@ -0,0 +1,6 @@
+git log
+git checkout ID index.html
+git add index.html
+git commit -m "done"
+git push
+git push origin master
---
+++ test5.sh
@@ -0,0 +1,10 @@
+cat /etc/passwd | cut -d: -f 1
+pwd
+su
+sudo
+hostname
+uname
+ls -ltr
+uptime
+rm -rf
+ip a
