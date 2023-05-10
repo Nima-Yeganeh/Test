2aae7d560bb158436ea9fdc17867cb40559576c2
root <yeganeh.nima.d@gmail.com>
2023-05-10 16:12:29 +0330

    done

---
+++ test2.sh
@@ -0,0 +1,8 @@
+git branch
+git remote -v
+git rm --cached index.html
+git branch test
+git checkout test
+git branch -d test
+git push --set-upstream origin test
+git pull --set-upstream origin test
