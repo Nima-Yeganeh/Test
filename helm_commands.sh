65c1e21cd6873dc1de6d853c66a49746f64e8cb6
nima-yeganeh <yeganeh.nima.d@gmail.com>
2023-05-11 16:06:39 +0000

    done

---
+++ helm_commands.sh
@@ -0,0 +1,9 @@
+helm repo list
+helm repo add bitnami https://charts.bitnami.com/bitnami
+helm repo add brigade https://brigadecore.github.io/charts
+helm search repo mysql
+helm search repo database
+helm search repo database --versions
+helm search hub nginx
+helm search hub nginx | wc -l
+
