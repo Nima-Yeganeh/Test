93e7dd9ac6a0fd91104e4be949820d5224fb5826
root <yeganeh.nima.d@gmail.com>
2023-05-11 18:49:39 +0330

    done

---
+++ docker_install_on_ubuntu.sh
@@ -0,0 +1,10 @@
+sudo apt update -y
+sudo apt-get install apt-transport-https ca-certificates curl gnupg lsb-release -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update -y
+sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+sudo apt install docker-compose -y
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo docker run hello-world
