7c7bec877409a4ce360e8fc7c1ebe15727b5e385
root <yeganeh.nima.d@gmail.com>
2023-05-13 16:50:15 +0330

    done

--- lamp_install_on_ubuntu.sh
+++ lamp_install_on_ubuntu.sh
@@ -13,26 +13,13 @@ sudo wget http://wordpress.org/latest.tar.gz
 ls
 sudo tar xzvf latest.tar.gz
 ls
-rm -f latest.tar.gz
+sudo rm -f latest.tar.gz
 sudo apt update -y
-sudo apt install php7.4 libssh2-php -y
-sudo mysql -u username -p -e "CREATE DATABASE wordpressdb;"
-sudo mysql -u username -p -e "CREATE USER wordpressuser@localhost IDENTIFIED BY 'P@ssw0rd';"
-sudo mysql -u username -p -e "GRANT ALL PRIVILEGES ON wordpressdb.* TO wordpressuser@localhost;"
-sudo mysql -u username -p -e "FLUSH PRIVILEGES;"
-echo '
-# BEGIN WordPress
-
-RewriteEngine On
-RewriteRule .* - [E=HTTP_AUTHORIZATION:%{HTTP:Authorization}]
-RewriteBase /
-RewriteRule ^index\.php$ - [L]
-RewriteCond %{REQUEST_FILENAME} !-f
-RewriteCond %{REQUEST_FILENAME} !-d
-RewriteRule . /index.php [L]
-
-# END WordPress
-' >> .htaccess
+# sudo apt install php7.4 libssh2-php -y
+sudo mysql -u root -e "CREATE DATABASE wordpressdb;"
+sudo mysql -u root -e "CREATE USER wordpressuser@localhost IDENTIFIED BY 'P@ssw0rd';"
+sudo mysql -u root -e "GRANT ALL PRIVILEGES ON wordpressdb.* TO wordpressuser@localhost;"
+sudo mysql -u root -e "FLUSH PRIVILEGES;"
 ls -anp
 sudo chown www-data:www-data .htaccess
 sudo service apache2 restart
