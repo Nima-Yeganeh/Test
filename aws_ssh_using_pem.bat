3667bb11cd3a34d59fb1f4042133396050e225a8
root <yeganeh.nima.d@gmail.com>
2023-05-13 22:04:17 +0330

    done

---
+++ aws_ssh_using_pem.bat
@@ -0,0 +1,9 @@
+@echo off
+
+set /p user="Enter your username: "
+set /p ip="Enter your IP address: "
+
+echo Username: %user%
+echo IP address: %ip%
+
+ssh -i aws_linux_key_for_test.pem %user%@%ip%
