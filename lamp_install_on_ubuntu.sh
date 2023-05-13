0e3c07a3339c084269cff138dffe0d14546329c5
root <yeganeh.nima.d@gmail.com>
2023-05-13 16:34:08 +0330

    done

--- lamp_install_on_ubuntu.sh
+++ lamp_install_on_ubuntu.sh
@@ -1,3 +1,37 @@
 sudo apt update -y
 sudo apt-get install tasksel -y
 sudo tasksel install lamp-server
+pwd
+cd /var/www/html
+pwd
+ls -anp
+sudo wget http://wordpress.org/latest.tar.gz
+ls
+sudo tar xzvf latest.tar.gz
+ls
+rm -f latest.tar.gz
+sudo apt update -y
+sudo apt install php7.4 libssh2-php -y
+sudo mysql -u username -p -e "CREATE DATABASE wordpressdb;"
+sudo mysql -u username -p -e "CREATE USER wordpressuser@localhost IDENTIFIED BY 'P@ssw0rd';"
+sudo mysql -u username -p -e "GRANT ALL PRIVILEGES ON wordpressdb.* TO wordpressuser@localhost;"
+sudo mysql -u username -p -e "FLUSH PRIVILEGES;"
+echo '
+# BEGIN WordPress
+
+RewriteEngine On
+RewriteRule .* - [E=HTTP_AUTHORIZATION:%{HTTP:Authorization}]
+RewriteBase /
+RewriteRule ^index\.php$ - [L]
+RewriteCond %{REQUEST_FILENAME} !-f
+RewriteCond %{REQUEST_FILENAME} !-d
+RewriteRule . /index.php [L]
+
+# END WordPress
+' >> .htaccess
+ls -anp
+sudo chown www-data:www-data .htaccess
+sudo service apache2 restart
+sudo chown www-data:www-data -R *
+sudo find . -type d -exec chmod 755 {} \;
+sudo find . -type d -exec chmod 644 {} \;
