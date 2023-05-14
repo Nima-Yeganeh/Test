c6fae873216d8b3b2a09913c5c58eee8d368cbc3
nima-yeganeh <yeganeh.nima.d@gmail.com>
2023-05-14 11:06:02 +0000

    done

--- mysql_connect_test.sh
+++ mysql_connect_test.sh
@@ -3,8 +3,10 @@
 # Prompt the user for the hostname, username, and password
 read -p "Enter the hostname: " hostname
 read -p "Enter the username: " username
+read -p "Enter the database: " database
 read -s -p "Enter the password: " password
 
 # Connect to the MySQL server and execute a command
-mysql -h $hostname -u $username -p$password
+mysql -h $hostname -u $username -p$password $database
+
 
