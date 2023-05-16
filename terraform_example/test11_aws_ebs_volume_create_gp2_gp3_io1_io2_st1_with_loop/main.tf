6292cd65ef2780fc1e481cdfc00c7d2d7fb85127
nima-yeganeh <yeganeh.nima.d@gmail.com>
2023-05-16 19:00:44 +0000

    done

---
+++ terraform_example/test11_aws_ebs_volume_create_gp2_gp3_io1_io2_st1_with_loop/main.tf
@@ -0,0 +1,51 @@
+provider "aws" {
+  region = "ap-southeast-2"
+}
+
+resource "aws_ebs_volume" "example_gb2" {
+  availability_zone = "ap-southeast-2a"
+  size              = 16000
+  type              = "gp2"
+  tags = {
+    Name = "example-gp2-volume"
+  }
+}
+
+resource "aws_ebs_volume" "example_gb3" {
+  availability_zone = "ap-southeast-2a"
+  size              = 16000
+  type              = "gp3"
+  tags = {
+    Name = "example-gp3-volume"
+  }
+}
+
+resource "aws_ebs_volume" "example_io1" {
+  availability_zone = "ap-southeast-2a"
+  size              = 16000
+  type              = "io1"
+  iops              = 1000
+  tags = {
+    Name = "example-io1-volume"
+  }
+}
+
+resource "aws_ebs_volume" "example_io2" {
+  availability_zone = "ap-southeast-2a"
+  size              = 16000
+  type              = "io2"
+  iops              = 2000
+  tags = {
+    Name = "example-io2-volume"
+  }
+}
+
+resource "aws_ebs_volume" "example_st1" {
+  availability_zone = "ap-southeast-2a"
+  size              = 16000
+  type              = "st1"
+  tags = {
+    Name = "example-st1-volume"
+  }
+}
+
