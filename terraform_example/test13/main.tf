e72f0d2ada4cc8b9e1b4716feff9cf6d9d5d0b5e
root <yeganeh.nima.d@gmail.com>
2023-05-18 13:56:12 +0330

    done

--- terraform_example/test13/main.tf
+++ terraform_example/test13/main.tf
@@ -1,12 +1,29 @@
 provider "aws" {
-  region = "us-east-1"
-  alias = "us-east-1"
+  region = "us-west-2"
+  alias = "west"
 }
 
-resource "aws_eip" "example" {
+resource "aws_eip" "example_us_west_2" {
   vpc = true
 
   tags = {
-    Name = "example-eip"
+    Name = "example-eip-us-west-2"
   }
+
+  provider = aws.west
+}
+
+provider "aws" {
+  region = "us-east-1"
+  alias = "east"
+}
+
+resource "aws_eip" "example_us_east_1" {
+  vpc = true
+
+  tags = {
+    Name = "example-eip-us-east-1"
+  }
+
+  provider = aws.east
 }
