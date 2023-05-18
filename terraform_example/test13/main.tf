200221ae531b1aa27c4306258cb92b84d081261c
root <yeganeh.nima.d@gmail.com>
2023-05-18 13:34:46 +0330

    done

--- terraform_example/test13/main.tf
+++ terraform_example/test13/main.tf
@@ -1,4 +1,112 @@
 provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_eip" "example" {
+  vpc = true
+
+  tags = {
+    Name = "example-eip"
+  }
+}
+
+provider "aws" {
+  region = "us-east-2"
+}
+
+resource "aws_eip" "example" {
+  vpc = true
+
+  tags = {
+    Name = "example-eip"
+  }
+}
+
+provider "aws" {
+  region = "us-west-1"
+}
+
+resource "aws_eip" "example" {
+  vpc = true
+
+  tags = {
+    Name = "example-eip"
+  }
+}
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_eip" "example" {
+  vpc = true
+
+  tags = {
+    Name = "example-eip"
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_eip" "example" {
+  vpc = true
+
+  tags = {
+    Name = "example-eip"
+  }
+}
+
+provider "aws" {
+  region = "ap-northeast-3"
+}
+
+resource "aws_eip" "example" {
+  vpc = true
+
+  tags = {
+    Name = "example-eip"
+  }
+}
+
+provider "aws" {
+  region = "ap-northeast-2"
+}
+
+resource "aws_eip" "example" {
+  vpc = true
+
+  tags = {
+    Name = "example-eip"
+  }
+}
+
+provider "aws" {
+  region = "ap-northeast-1"
+}
+
+resource "aws_eip" "example" {
+  vpc = true
+
+  tags = {
+    Name = "example-eip"
+  }
+}
+
+provider "aws" {
+  region = "ap-southeast-1"
+}
+
+resource "aws_eip" "example" {
+  vpc = true
+
+  tags = {
+    Name = "example-eip"
+  }
+}
+
+provider "aws" {
   region = "ap-southeast-2"
 }
 
@@ -10,3 +118,86 @@ resource "aws_eip" "example" {
   }
 }
 
+provider "aws" {
+  region = "ca-central-1"
+}
+
+resource "aws_eip" "example" {
+  vpc = true
+
+  tags = {
+    Name = "example-eip"
+  }
+}
+
+provider "aws" {
+  region = "eu-central-1"
+}
+
+resource "aws_eip" "example" {
+  vpc = true
+
+  tags = {
+    Name = "example-eip"
+  }
+}
+
+provider "aws" {
+  region = "eu-west-1"
+}
+
+resource "aws_eip" "example" {
+  vpc = true
+
+  tags = {
+    Name = "example-eip"
+  }
+}
+
+provider "aws" {
+  region = "eu-west-2"
+}
+
+resource "aws_eip" "example" {
+  vpc = true
+
+  tags = {
+    Name = "example-eip"
+  }
+}
+
+provider "aws" {
+  region = "eu-west-3"
+}
+
+resource "aws_eip" "example" {
+  vpc = true
+
+  tags = {
+    Name = "example-eip"
+  }
+}
+
+provider "aws" {
+  region = "eu-north-1"
+}
+
+resource "aws_eip" "example" {
+  vpc = true
+
+  tags = {
+    Name = "example-eip"
+  }
+}
+
+provider "aws" {
+  region = "sa-east-1"
+}
+
+resource "aws_eip" "example" {
+  vpc = true
+
+  tags = {
+    Name = "example-eip"
+  }
+}
