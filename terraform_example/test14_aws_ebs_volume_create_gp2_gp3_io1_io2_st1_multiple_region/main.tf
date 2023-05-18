9d25f6267dd9470c7816abb3bfbcb7343c1fd406
root <yeganeh.nima.d@gmail.com>
2023-05-18 14:48:52 +0330

    done

--- terraform_example/test14_aws_ebs_volume_create_gp2_gp3_io1_io2_st1_multiple_region/main.tf
+++ terraform_example/test14_aws_ebs_volume_create_gp2_gp3_io1_io2_st1_multiple_region/main.tf
@@ -55,3 +55,60 @@ resource "aws_ebs_volume" "example_st1" {
   provider = aws.us-east-1
 }
 
+provider "aws" {
+  region = "us-east-2"
+  alias = "us-east-2"
+}
+
+resource "aws_ebs_volume" "example_gb2" {
+  availability_zone = "us-east-2a"
+  size              = 16000
+  type              = "gp2"
+  tags = {
+    Name = "example-gp2-volume"
+  }
+  provider = aws.us-east-2
+}
+
+resource "aws_ebs_volume" "example_gb3" {
+  availability_zone = "us-east-2a"
+  size              = 16000
+  type              = "gp3"
+  tags = {
+    Name = "example-gp3-volume"
+  }
+  provider = aws.us-east-2
+}
+
+resource "aws_ebs_volume" "example_io1" {
+  availability_zone = "us-east-2a"
+  size              = 16000
+  type              = "io1"
+  iops              = 1000
+  tags = {
+    Name = "example-io1-volume"
+  }
+  provider = aws.us-east-2
+}
+
+resource "aws_ebs_volume" "example_io2" {
+  availability_zone = "us-east-2a"
+  size              = 16000
+  type              = "io2"
+  iops              = 2000
+  tags = {
+    Name = "example-io2-volume"
+  }
+  provider = aws.us-east-2
+}
+
+resource "aws_ebs_volume" "example_st1" {
+  availability_zone = "us-east-2a"
+  size              = 16000
+  type              = "st1"
+  tags = {
+    Name = "example-st1-volume"
+  }
+  provider = aws.us-east-2
+}
+
