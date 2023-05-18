4403701697929d68e7d53a983c42e5bbc0299b1f
root <yeganeh.nima.d@gmail.com>
2023-05-18 17:03:36 +0330

    done

--- terraform_example/test18_aws_rds_db_instance_create_mariadb_db.x2iedn.32xlarge_multiple_instance_region/run.sh
+++ terraform_example/test18_aws_rds_db_instance_create_mariadb_db.x2iedn.32xlarge_multiple_instance_region/run.sh
@@ -1,6 +1,6 @@
 terraform init
 terraform plan
-terraform apply -auto-approve -parallelism=200
+terraform apply -auto-approve -parallelism=40
 rm -rf .terraform
 rm -f .terraform.lock.hcl
 rm -f terraform.tfstate
