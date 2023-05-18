b58975ec118e54076d05210d1d5b64bb1872188a
root <yeganeh.nima.d@gmail.com>
2023-05-18 22:05:54 +0330

    done

--- terraform_example/test23_aws_elasticache_cluster_memcached_cache.r5.24xlarge_multi_instance_region/main.tf
+++ terraform_example/test23_aws_elasticache_cluster_memcached_cache.r5.24xlarge_multi_instance_region/main.tf
@@ -93,176 +93,176 @@ variable "regions" {
 }
 
 resource "aws_elasticache_parameter_group" "example" {
-  name   = "example-test10"
+  name   = "example-test12"
   family = "memcached1.6"
 }
 
 resource "aws_elasticache_cluster" "example1" {
   for_each             = toset(var.regions)
-  cluster_id           = "test-cluster-${each.key}"
+  cluster_id           = "testi-cluster-${each.key}"
   engine               = "memcached"
   node_type            = "cache.r5.24xlarge"
-  num_cache_nodes      = 4
+  num_cache_nodes      = 8
   port                 = 11211
   provider             = aws.aws1
 }
 
 resource "aws_elasticache_cluster" "example2" {
   for_each             = toset(var.regions)
-  cluster_id           = "test-cluster-${each.key}"
+  cluster_id           = "testi-cluster-${each.key}"
   engine               = "memcached"
   node_type            = "cache.r5.24xlarge"
-  num_cache_nodes      = 4
+  num_cache_nodes      = 8
   port                 = 11211
   provider             = aws.aws2
 }
 
 resource "aws_elasticache_cluster" "example3" {
   for_each             = toset(var.regions)
-  cluster_id           = "test-cluster-${each.key}"
+  cluster_id           = "testi-cluster-${each.key}"
   engine               = "memcached"
   node_type            = "cache.r5.24xlarge"
-  num_cache_nodes      = 4
+  num_cache_nodes      = 8
   port                 = 11211
   provider             = aws.aws3
 }
 
 resource "aws_elasticache_cluster" "example4" {
   for_each             = toset(var.regions)
-  cluster_id           = "test-cluster-${each.key}"
+  cluster_id           = "testi-cluster-${each.key}"
   engine               = "memcached"
   node_type            = "cache.r5.24xlarge"
-  num_cache_nodes      = 4
+  num_cache_nodes      = 8
   port                 = 11211
   provider             = aws.aws4
 }
 
 resource "aws_elasticache_cluster" "example5" {
   for_each             = toset(var.regions)
-  cluster_id           = "test-cluster-${each.key}"
+  cluster_id           = "testi-cluster-${each.key}"
   engine               = "memcached"
   node_type            = "cache.r5.24xlarge"
-  num_cache_nodes      = 4
+  num_cache_nodes      = 8
   port                 = 11211
   provider             = aws.aws5
 }
 
 resource "aws_elasticache_cluster" "example6" {
   for_each             = toset(var.regions)
-  cluster_id           = "test-cluster-${each.key}"
+  cluster_id           = "testi-cluster-${each.key}"
   engine               = "memcached"
   node_type            = "cache.r5.24xlarge"
-  num_cache_nodes      = 4
+  num_cache_nodes      = 8
   port                 = 11211
   provider             = aws.aws6
 }
 
 resource "aws_elasticache_cluster" "example7" {
   for_each             = toset(var.regions)
-  cluster_id           = "test-cluster-${each.key}"
+  cluster_id           = "testi-cluster-${each.key}"
   engine               = "memcached"
   node_type            = "cache.r5.24xlarge"
-  num_cache_nodes      = 4
+  num_cache_nodes      = 8
   port                 = 11211
   provider             = aws.aws7
 }
 
 resource "aws_elasticache_cluster" "example8" {
   for_each             = toset(var.regions)
-  cluster_id           = "test-cluster-${each.key}"
+  cluster_id           = "testi-cluster-${each.key}"
   engine               = "memcached"
   node_type            = "cache.r5.24xlarge"
-  num_cache_nodes      = 4
+  num_cache_nodes      = 8
   port                 = 11211
   provider             = aws.aws8
 }
 
 resource "aws_elasticache_cluster" "example9" {
   for_each             = toset(var.regions)
-  cluster_id           = "test-cluster-${each.key}"
+  cluster_id           = "testi-cluster-${each.key}"
   engine               = "memcached"
   node_type            = "cache.r5.24xlarge"
-  num_cache_nodes      = 4
+  num_cache_nodes      = 8
   port                 = 11211
   provider             = aws.aws9
 }
 
 resource "aws_elasticache_cluster" "example10" {
   for_each             = toset(var.regions)
-  cluster_id           = "test-cluster-${each.key}"
+  cluster_id           = "testi-cluster-${each.key}"
   engine               = "memcached"
   node_type            = "cache.r5.24xlarge"
-  num_cache_nodes      = 4
+  num_cache_nodes      = 8
   port                 = 11211
   provider             = aws.aws10
 }
 
 resource "aws_elasticache_cluster" "example11" {
   for_each             = toset(var.regions)
-  cluster_id           = "test-cluster-${each.key}"
+  cluster_id           = "testi-cluster-${each.key}"
   engine               = "memcached"
   node_type            = "cache.r5.24xlarge"
-  num_cache_nodes      = 4
+  num_cache_nodes      = 8
   port                 = 11211
   provider             = aws.aws11
 }
 
 resource "aws_elasticache_cluster" "example12" {
   for_each             = toset(var.regions)
-  cluster_id           = "test-cluster-${each.key}"
+  cluster_id           = "testi-cluster-${each.key}"
   engine               = "memcached"
   node_type            = "cache.r5.24xlarge"
-  num_cache_nodes      = 4
+  num_cache_nodes      = 8
   port                 = 11211
   provider             = aws.aws12
 }
 
 resource "aws_elasticache_cluster" "example13" {
   for_each             = toset(var.regions)
-  cluster_id           = "test-cluster-${each.key}"
+  cluster_id           = "testi-cluster-${each.key}"
   engine               = "memcached"
   node_type            = "cache.r5.24xlarge"
-  num_cache_nodes      = 4
+  num_cache_nodes      = 8
   port                 = 11211
   provider             = aws.aws13
 }
 
 resource "aws_elasticache_cluster" "example14" {
   for_each             = toset(var.regions)
-  cluster_id           = "test-cluster-${each.key}"
+  cluster_id           = "testi-cluster-${each.key}"
   engine               = "memcached"
   node_type            = "cache.r5.24xlarge"
-  num_cache_nodes      = 4
+  num_cache_nodes      = 8
   port                 = 11211
   provider             = aws.aws14
 }
 
 resource "aws_elasticache_cluster" "example15" {
   for_each             = toset(var.regions)
-  cluster_id           = "test-cluster-${each.key}"
+  cluster_id           = "testi-cluster-${each.key}"
   engine               = "memcached"
   node_type            = "cache.r5.24xlarge"
-  num_cache_nodes      = 4
+  num_cache_nodes      = 8
   port                 = 11211
   provider             = aws.aws15
 }
 
 resource "aws_elasticache_cluster" "example16" {
   for_each             = toset(var.regions)
-  cluster_id           = "test-cluster-${each.key}"
+  cluster_id           = "testi-cluster-${each.key}"
   engine               = "memcached"
   node_type            = "cache.r5.24xlarge"
-  num_cache_nodes      = 4
+  num_cache_nodes      = 8
   port                 = 11211
   provider             = aws.aws16
 }
 
 resource "aws_elasticache_cluster" "example17" {
   for_each             = toset(var.regions)
-  cluster_id           = "test-cluster-${each.key}"
+  cluster_id           = "testi-cluster-${each.key}"
   engine               = "memcached"
   node_type            = "cache.r5.24xlarge"
-  num_cache_nodes      = 4
+  num_cache_nodes      = 8
   port                 = 11211
   provider             = aws.aws17
 }
