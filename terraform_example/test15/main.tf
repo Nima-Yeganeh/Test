3e2316e717edfd02c58a16c2a9522e4d5e49ae5a
root <yeganeh.nima.d@gmail.com>
2023-05-18 15:31:29 +0330

    done

--- terraform_example/test15/main.tf
+++ terraform_example/test15/main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-    region = "eu-west-2"
+    region = "eu-west-1"
 }
 
 resource "aws_db_instance" "myRDS" {
@@ -7,9 +7,9 @@ resource "aws_db_instance" "myRDS" {
     identifier = "my-first-rds"
     instance_class = "db.t2.micro"
     engine = "mariadb"
-    engine_version = "10.2.21"
-    username = "bob"
-    password = "password123"
+    engine_version = "10.16.10"
+    username = "nima"
+    password = "password1234"
     port = "3306"
     allocated_storage = "20"
     skip_final_snapshot = true
