993f93a5fdc6ffdfb4d395ee3ea9a0ca1dfcdec6
root <yeganeh.nima.d@gmail.com>
2023-05-18 18:07:17 +0330

    done

--- terraform_example/test19_aws_rds_db_instance_create_mariadb_db.x2iedn.32xlarge_multiple_instance_region_parallel/main.tf
+++ terraform_example/test19_aws_rds_db_instance_create_mariadb_db.x2iedn.32xlarge_multiple_instance_region_parallel/main.tf
@@ -3,27 +3,6 @@ provider "aws" {
   alias = "aws1"
 }
 
-resource "aws_iam_role" "myrds_monitoring" {
-  name = "myrds_monitoring"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Principal = {
-          Service = "monitoring.rds.amazonaws.com"
-        }
-        Action = "sts:AssumeRole"
-      }
-    ]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "myrds_monitoring" {
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonRDSEnhancedMonitoringRole"
-  role = aws_iam_role.myrds_monitoring.name
-}
-
 resource "aws_db_instance" "mynewtestirds_example1" {
   count = 20
   name = "mynewtestirds${count.index + 1}"
@@ -42,11 +21,9 @@ resource "aws_db_instance" "mynewtestirds_example1" {
   performance_insights_enabled = true
   multi_az = true
   monitoring_interval = 1
-  monitoring_role_arn = aws_iam_role.myrds_monitoring.arn
   enabled_cloudwatch_logs_exports = ["audit", "error", "general", "slowquery"]
   storage_encrypted = true
   storage_type = "gp2"
   max_allocated_storage = 20
   provider = aws.aws1
 }
-
