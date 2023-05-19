fcf0e17a536890e5105b870a9b703d5160f6e53c
root <yeganeh.nima.d@gmail.com>
2023-05-19 12:46:36 +0330

    done

--- terraform_example/test29_aws_opensearch_domain_elasticsearch_create_r6gd.12xlarge.search_multi_az/main.tf
+++ terraform_example/test29_aws_opensearch_domain_elasticsearch_create_r6gd.12xlarge.search_multi_az/main.tf
@@ -23,9 +23,6 @@ resource "aws_opensearch_service_domain" "example_domain_aws1" {
   snapshot_options {
     automated_snapshot_start_hour = 23
   }
-  advanced_options {
-    rest.action.multi.allow_explicit_index = "true"
-  }
   provider = aws.aws1
 }
 output "example_domain_aws1_endpoint" {
