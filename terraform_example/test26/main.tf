00eb29dac9d8de5001827ce378712436fb0828b2
root <yeganeh.nima.d@gmail.com>
2023-05-19 11:29:47 +0330

    done

--- terraform_example/test26/main.tf
+++ terraform_example/test26/main.tf
@@ -9,31 +9,27 @@ resource "aws_apigatewayv2_api" "example_api" {
 }
 
 resource "aws_apigatewayv2_route" "example_route" {
-  count     = 20
-  api_id    = aws_apigatewayv2_api.example_api[count.index].id
+  api_id    = aws_apigatewayv2_api.example_api.id
   route_key = "GET /example"
 }
 
 resource "aws_apigatewayv2_integration" "example_integration" {
-  count             = 20
-  api_id            = aws_apigatewayv2_api.example_api[count.index].id
+  api_id            = aws_apigatewayv2_api.example_api.id
   integration_type  = "HTTP_PROXY"
   integration_uri   = "https://jsonplaceholder.typicode.com"
   integration_method = "GET"
   
   depends_on = [
-    aws_apigatewayv2_route.example_route[count.index],
+    aws_apigatewayv2_route.example_route,
   ]
 }
 
 resource "aws_apigatewayv2_stage" "example_stage" {
-  count       = 20
-  api_id      = aws_apigatewayv2_api.example_api[count.index].id
+  api_id      = aws_apigatewayv2_api.example_api.id
   name        = "dev"
   auto_deploy = true
   
   depends_on = [
-aws_apigatewayv2_integration.example_integration[${count.index}],
-    aws_apigatewayv2_route.example_route[${count.index}],
+    aws_apigatewayv2_integration.example_integration,
   ]
 }
