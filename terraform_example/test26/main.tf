35552006c13bcbdc020e8d49a45f2ab7fe717f60
root <yeganeh.nima.d@gmail.com>
2023-05-19 11:36:31 +0330

    done

--- terraform_example/test26/main.tf
+++ terraform_example/test26/main.tf
@@ -3,6 +3,86 @@ provider "aws" {
   alias = "aws1"
 }
 
+provider "aws" {
+  region = "us-east-2"
+  alias = "aws2"
+}
+
+provider "aws" {
+  region = "us-west-1"
+  alias = "aws3"
+}
+
+provider "aws" {
+  region = "us-west-2"
+  alias = "aws4"
+}
+
+provider "aws" {
+  region = "ap-south-1"
+  alias = "aws5"
+}
+
+provider "aws" {
+  region = "ap-northeast-1"
+  alias = "aws6"
+}
+
+provider "aws" {
+  region = "ap-northeast-2"
+  alias = "aws7"
+}
+
+provider "aws" {
+  region = "ap-northeast-3"
+  alias = "aws8"
+}
+
+provider "aws" {
+  region = "ap-southeast-1"
+  alias = "aws9"
+}
+
+provider "aws" {
+  region = "ap-southeast-2"
+  alias = "aws10"
+}
+
+provider "aws" {
+  region = "ca-central-1"
+  alias = "aws11"
+}
+
+provider "aws" {
+  region = "eu-central-1"
+  alias = "aws12"
+}
+
+provider "aws" {
+  region = "eu-west-1"
+  alias = "aws13"
+}
+
+provider "aws" {
+  region = "eu-west-2"
+  alias = "aws14"
+}
+
+provider "aws" {
+  region = "eu-west-3"
+  alias = "aws15"
+}
+
+provider "aws" {
+  region = "eu-north-1"
+  alias = "aws16"
+}
+
+provider "aws" {
+  region = "sa-east-1"
+  alias = "aws17"
+}
+
 resource "aws_apigatewayv2_api" "example_api" {
   name          = "example-api"
   protocol_type = "HTTP"
