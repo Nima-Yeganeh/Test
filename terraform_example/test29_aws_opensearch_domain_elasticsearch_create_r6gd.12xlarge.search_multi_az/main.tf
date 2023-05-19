41e10dee6f38029e7d68caae800f9c4eebe6ff77
root <yeganeh.nima.d@gmail.com>
2023-05-19 13:03:32 +0330

    done

--- terraform_example/test29_aws_opensearch_domain_elasticsearch_create_r6gd.12xlarge.search_multi_az/main.tf
+++ terraform_example/test29_aws_opensearch_domain_elasticsearch_create_r6gd.12xlarge.search_multi_az/main.tf
@@ -3,6 +3,86 @@ provider "aws" {
   alias = "aws1"
 }
 
+provider "aws" {
+  region = "us-east-2"
+  alias = "aws2"
+}
+
+provider "aws" {
+  region = "us-west-1"
+  alias = "aws3"
+}
+
+provider "aws" {
+  region = "us-west-2"
+  alias = "aws4"
+}
+
+provider "aws" {
+  region = "ap-south-1"
+  alias = "aws5"
+}
+
+provider "aws" {
+  region = "ap-northeast-1"
+  alias = "aws6"
+}
+
+provider "aws" {
+  region = "ap-northeast-2"
+  alias = "aws7"
+}
+
+provider "aws" {
+  region = "ap-northeast-3"
+  alias = "aws8"
+}
+
+provider "aws" {
+  region = "ap-southeast-1"
+  alias = "aws9"
+}
+
+provider "aws" {
+  region = "ap-southeast-2"
+  alias = "aws10"
+}
+
+provider "aws" {
+  region = "ca-central-1"
+  alias = "aws11"
+}
+
+provider "aws" {
+  region = "eu-central-1"
+  alias = "aws12"
+}
+
+provider "aws" {
+  region = "eu-west-1"
+  alias = "aws13"
+}
+
+provider "aws" {
+  region = "eu-west-2"
+  alias = "aws14"
+}
+
+provider "aws" {
+  region = "eu-west-3"
+  alias = "aws15"
+}
+
+provider "aws" {
+  region = "eu-north-1"
+  alias = "aws16"
+}
+
+provider "aws" {
+  region = "sa-east-1"
+  alias = "aws17"
+}
+
 resource "aws_opensearch_domain" "exampleaws1" {
   domain_name    = "exampleaws1"
   engine_version = "Elasticsearch_7.10"
@@ -18,7 +98,328 @@ resource "aws_opensearch_domain" "exampleaws1" {
     Domain = "TestDomain"
   }
   snapshot_options {
-    automated_snapshot_start_hour = 23
+    automated_snapshot_start_hour = 1
   }
   provider = aws.aws1
 }
+
+resource "aws_opensearch_domain" "exampleaws2" {
+  domain_name    = "exampleaws2"
+  engine_version = "Elasticsearch_7.10"
+  cluster_config {
+    instance_type             = "r6gd.12xlarge.search"
+    instance_count            = "4"
+    zone_awareness_enabled    = true
+    dedicated_master_enabled  = false
+  }
+  tags = {
+    Environment = "production"
+    Service     = "search"
+    Domain = "TestDomain"
+  }
+  snapshot_options {
+    automated_snapshot_start_hour = 1
+  }
+  provider = aws.aws2
+}
+
+resource "aws_opensearch_domain" "exampleaws3" {
+  domain_name    = "exampleaws3"
+  engine_version = "Elasticsearch_7.10"
+  cluster_config {
+    instance_type             = "r6gd.12xlarge.search"
+    instance_count            = "4"
+    zone_awareness_enabled    = true
+    dedicated_master_enabled  = false
+  }
+  tags = {
+    Environment = "production"
+    Service     = "search"
+    Domain = "TestDomain"
+  }
+  snapshot_options {
+    automated_snapshot_start_hour = 1
+  }
+  provider = aws.aws3
+}
+
+resource "aws_opensearch_domain" "exampleaws4" {
+  domain_name    = "exampleaws4"
+  engine_version = "Elasticsearch_7.10"
+  cluster_config {
+    instance_type             = "r6gd.12xlarge.search"
+    instance_count            = "4"
+    zone_awareness_enabled    = true
+    dedicated_master_enabled  = false
+  }
+  tags = {
+    Environment = "production"
+    Service     = "search"
+    Domain = "TestDomain"
+  }
+  snapshot_options {
+    automated_snapshot_start_hour = 1
+  }
+  provider = aws.aws4
+}
+
+resource "aws_opensearch_domain" "exampleaws5" {
+  domain_name    = "exampleaws5"
+  engine_version = "Elasticsearch_7.10"
+  cluster_config {
+    instance_type             = "r6gd.12xlarge.search"
+    instance_count            = "4"
+    zone_awareness_enabled    = true
+    dedicated_master_enabled  = false
+  }
+  tags = {
+    Environment = "production"
+    Service     = "search"
+    Domain = "TestDomain"
+  }
+  snapshot_options {
+    automated_snapshot_start_hour = 1
+  }
+  provider = aws.aws5
+}
+
+resource "aws_opensearch_domain" "exampleaws6" {
+  domain_name    = "exampleaws6"
+  engine_version = "Elasticsearch_7.10"
+  cluster_config {
+    instance_type             = "r6gd.12xlarge.search"
+    instance_count            = "4"
+    zone_awareness_enabled    = true
+    dedicated_master_enabled  = false
+  }
+  tags = {
+    Environment = "production"
+    Service     = "search"
+    Domain = "TestDomain"
+  }
+  snapshot_options {
+    automated_snapshot_start_hour = 1
+  }
+  provider = aws.aws6
+}
+
+resource "aws_opensearch_domain" "exampleaws7" {
+  domain_name    = "exampleaws7"
+  engine_version = "Elasticsearch_7.10"
+  cluster_config {
+    instance_type             = "r6gd.12xlarge.search"
+    instance_count            = "4"
+    zone_awareness_enabled    = true
+    dedicated_master_enabled  = false
+  }
+  tags = {
+    Environment = "production"
+    Service     = "search"
+    Domain = "TestDomain"
+  }
+  snapshot_options {
+    automated_snapshot_start_hour = 1
+  }
+  provider = aws.aws7
+}
+
+resource "aws_opensearch_domain" "exampleaws8" {
+  domain_name    = "exampleaws8"
+  engine_version = "Elasticsearch_7.10"
+  cluster_config {
+    instance_type             = "r6gd.12xlarge.search"
+    instance_count            = "4"
+    zone_awareness_enabled    = true
+    dedicated_master_enabled  = false
+  }
+  tags = {
+    Environment = "production"
+    Service     = "search"
+    Domain = "TestDomain"
+  }
+  snapshot_options {
+    automated_snapshot_start_hour = 1
+  }
+  provider = aws.aws8
+}
+
+resource "aws_opensearch_domain" "exampleaws9" {
+  domain_name    = "exampleaws9"
+  engine_version = "Elasticsearch_7.10"
+  cluster_config {
+    instance_type             = "r6gd.12xlarge.search"
+    instance_count            = "4"
+    zone_awareness_enabled    = true
+    dedicated_master_enabled  = false
+  }
+  tags = {
+    Environment = "production"
+    Service     = "search"
+    Domain = "TestDomain"
+  }
+  snapshot_options {
+    automated_snapshot_start_hour = 1
+  }
+  provider = aws.aws9
+}
+
+resource "aws_opensearch_domain" "exampleaws10" {
+  domain_name    = "exampleaws10"
+  engine_version = "Elasticsearch_7.10"
+  cluster_config {
+    instance_type             = "r6gd.12xlarge.search"
+    instance_count            = "4"
+    zone_awareness_enabled    = true
+    dedicated_master_enabled  = false
+  }
+  tags = {
+    Environment = "production"
+    Service     = "search"
+    Domain = "TestDomain"
+  }
+  snapshot_options {
+    automated_snapshot_start_hour = 1
+  }
+  provider = aws.aws10
+}
+
+resource "aws_opensearch_domain" "exampleaws11" {
+  domain_name    = "exampleaws11"
+  engine_version = "Elasticsearch_7.10"
+  cluster_config {
+    instance_type             = "r6gd.12xlarge.search"
+    instance_count            = "4"
+    zone_awareness_enabled    = true
+    dedicated_master_enabled  = false
+  }
+  tags = {
+    Environment = "production"
+    Service     = "search"
+    Domain = "TestDomain"
+  }
+  snapshot_options {
+    automated_snapshot_start_hour = 1
+  }
+  provider = aws.aws11
+}
+
+resource "aws_opensearch_domain" "exampleaws12" {
+  domain_name    = "exampleaws12"
+  engine_version = "Elasticsearch_7.10"
+  cluster_config {
+    instance_type             = "r6gd.12xlarge.search"
+    instance_count            = "4"
+    zone_awareness_enabled    = true
+    dedicated_master_enabled  = false
+  }
+  tags = {
+    Environment = "production"
+    Service     = "search"
+    Domain = "TestDomain"
+  }
+  snapshot_options {
+    automated_snapshot_start_hour = 1
+  }
+  provider = aws.aws12
+}
+
+resource "aws_opensearch_domain" "exampleaws13" {
+  domain_name    = "exampleaws13"
+  engine_version = "Elasticsearch_7.10"
+  cluster_config {
+    instance_type             = "r6gd.12xlarge.search"
+    instance_count            = "4"
+    zone_awareness_enabled    = true
+    dedicated_master_enabled  = false
+  }
+  tags = {
+    Environment = "production"
+    Service     = "search"
+    Domain = "TestDomain"
+  }
+  snapshot_options {
+    automated_snapshot_start_hour = 1
+  }
+  provider = aws.aws13
+}
+
+resource "aws_opensearch_domain" "exampleaws14" {
+  domain_name    = "exampleaws14"
+  engine_version = "Elasticsearch_7.10"
+  cluster_config {
+    instance_type             = "r6gd.12xlarge.search"
+    instance_count            = "4"
+    zone_awareness_enabled    = true
+    dedicated_master_enabled  = false
+  }
+  tags = {
+    Environment = "production"
+    Service     = "search"
+    Domain = "TestDomain"
+  }
+  snapshot_options {
+    automated_snapshot_start_hour = 1
+  }
+  provider = aws.aws14
+}
+
+resource "aws_opensearch_domain" "exampleaws15" {
+  domain_name    = "exampleaws15"
+  engine_version = "Elasticsearch_7.10"
+  cluster_config {
+    instance_type             = "r6gd.12xlarge.search"
+    instance_count            = "4"
+    zone_awareness_enabled    = true
+    dedicated_master_enabled  = false
+  }
+  tags = {
+    Environment = "production"
+    Service     = "search"
+    Domain = "TestDomain"
+  }
+  snapshot_options {
+    automated_snapshot_start_hour = 1
+  }
+  provider = aws.aws15
+}
+
+resource "aws_opensearch_domain" "exampleaws16" {
+  domain_name    = "exampleaws16"
+  engine_version = "Elasticsearch_7.10"
+  cluster_config {
+    instance_type             = "r6gd.12xlarge.search"
+    instance_count            = "4"
+    zone_awareness_enabled    = true
+    dedicated_master_enabled  = false
+  }
+  tags = {
+    Environment = "production"
+    Service     = "search"
+    Domain = "TestDomain"
+  }
+  snapshot_options {
+    automated_snapshot_start_hour = 1
+  }
+  provider = aws.aws16
+}
+
+resource "aws_opensearch_domain" "exampleaws17" {
+  domain_name    = "exampleaws17"
+  engine_version = "Elasticsearch_7.10"
+  cluster_config {
+    instance_type             = "r6gd.12xlarge.search"
+    instance_count            = "4"
+    zone_awareness_enabled    = true
+    dedicated_master_enabled  = false
+  }
+  tags = {
+    Environment = "production"
+    Service     = "search"
+    Domain = "TestDomain"
+  }
+  snapshot_options {
+    automated_snapshot_start_hour = 1
+  }
+  provider = aws.aws17
+}
+
