627b488670ff9fdf71d20bbf15d87ecb9e49c75f
root <yeganeh.nima.d@gmail.com>
2023-05-19 12:56:02 +0330

    done

--- terraform_example/test29_aws_opensearch_domain_elasticsearch_create_r6gd.12xlarge.search_multi_az/main.tf
+++ terraform_example/test29_aws_opensearch_domain_elasticsearch_create_r6gd.12xlarge.search_multi_az/main.tf
@@ -7,9 +7,10 @@ resource "aws_opensearch_domain" "exampleaws1" {
   domain_name    = "exampleaws1"
   engine_version = "Elasticsearch_7.10"
   cluster_config {
-    instance_type           = "r6gd.12xlarge.search"
-    instance_count          = "3"
-    zone_awareness_enabled  = true
+    instance_type             = "r6gd.12xlarge.search"
+    instance_count            = "4"
+    zone_awareness_enabled    = true
+    dedicated_master_enabled  = true
   }
   ebs_options {
     ebs_enabled = true
