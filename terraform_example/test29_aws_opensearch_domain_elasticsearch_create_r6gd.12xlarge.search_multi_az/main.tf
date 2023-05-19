33a534dbd63dd46df6f79356d661f5cab2e2c96f
root <yeganeh.nima.d@gmail.com>
2023-05-19 12:45:38 +0330

    done

--- terraform_example/test29_aws_opensearch_domain_elasticsearch_create_r6gd.12xlarge.search_multi_az/main.tf
+++ terraform_example/test29_aws_opensearch_domain_elasticsearch_create_r6gd.12xlarge.search_multi_az/main.tf
@@ -9,30 +9,25 @@ resource "aws_opensearch_service_domain" "example_domain_aws1" {
   instance_type         = "r6gd.12xlarge.search"
   node_count            = 3
   zone_awareness_enabled = true
-  
   ebs_options {
     ebs_enabled = true
     volume_size = 100
   }
-  
   encryption_at_rest {
     enabled = true
   }
-  
   tags = {
     Environment = "production"
     Service     = "search"
   }
-  
   snapshot_options {
     automated_snapshot_start_hour = 23
   }
-  
   advanced_options {
     "rest.action.multi.allow_explicit_index" = "true"
   }
+  provider = aws.aws1
 }
-
 output "example_domain_aws1_endpoint" {
   value = aws_opensearch_service_domain.example_domain_aws1.endpoint
 }
