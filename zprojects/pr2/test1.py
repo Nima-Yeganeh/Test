ceee88e1abe6f40830e599937339423cfe7ac3a4
root <yeganeh.nima.d@gmail.com>
2023-05-21 17:37:13 +0330

    done

--- zprojects/pr2/test1.py
+++ zprojects/pr2/test1.py
@@ -85,7 +85,7 @@ def generate_response(question):
 
 def generate_filename(newfilename):
     original_string1 = __file__
-    new_string1 = original_string1.replace(os.path.basename(__file__), newfilename+'.MD')
+    new_string1 = original_string1.replace(os.path.basename(__file__), 'data/'+newfilename+'.MD')
     # newfile_path1 = new_string1
     newfile_path1 = new_string1.replace(" ", "_")
     return(newfile_path1)
