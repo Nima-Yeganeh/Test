fd8a767836bf78f065b6d5999df802f4d0a7b4d5
root <yeganeh.nima.d@gmail.com>
2023-05-21 11:36:49 +0330

    done

---
+++ insta/test10.py
@@ -0,0 +1,23 @@
+from instagrapi import Client
+from instagrapi.exceptions import ClientLoginRequired
+
+# Set up your instagrapi session
+client = Client()
+client.login("nimaxyeganeh", "")
+
+# Define the Instagram tag to search for
+tag = "food"
+
+# Find posts with the given tag
+try:
+    media_list = client.search_tag(tag)
+    for media in media_list:
+        # Follow the user
+        client.user_follow(media.user.pk)
+        # Like the post
+        client.post_like(media.pk)
+except ClientLoginRequired as e:
+    print(f"Could not log in: {e}")
+
+# Logout of your Instagram account
+client.logout()
