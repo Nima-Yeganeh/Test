d24fc568c67c6ccb1d57def804a4c2b7350c758b
root <yeganeh.nima.d@gmail.com>
2023-05-24 09:59:09 +0330

    done

---
+++ zprojects/pr6/renamefiles.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+zstr="Tell_me_about_devops_and_microservice_"
+
+# Get a list of files in the current directory
+files=$(ls data/)
+# echo $files
+
+# Loop through each file
+for file in $files; do
+	# Check if the file name contains the string to remove
+	#echo $file
+	if echo $file | grep -q "$zstr"; then
+		#echo $file
+		# Remove the string from the file name
+		new_file=$(echo $file | sed "s/$zstr//")
+		# Rename the file
+		echo $new_file
+		mv data/$file data/$new_file
+	fi
+done
+
