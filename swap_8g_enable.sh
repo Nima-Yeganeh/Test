f284fb5d1b77fb2c64f1faa4383619d6f0192790
nima-yeganeh <yeganeh.nima.d@gmail.com>
2023-05-27 06:53:47 +0000

    done

---
+++ swap_8g_enable.sh
@@ -0,0 +1,7 @@
+sudo fallocate -l 8G /swapfile
+ls -anp /swapfile
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
+echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
+
