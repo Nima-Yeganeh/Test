2104b1928e97e65f4600429894b0613652c5514e
nima-yeganeh <yeganeh.nima.d@gmail.com>
2023-05-27 08:26:47 +0000

    done

--- docker/test2_php/src/index.php
+++ docker/test2_php/src/index.php
@@ -3,6 +3,7 @@
   $url = 'https://raw.githubusercontent.com/Nima-Yeganeh/Test/main/index.html';
   $html = file_get_contents($url);
   echo $html;
+  echo "<br>";
   $url = 'https://raw.githubusercontent.com/Nima-Yeganeh/Test/main/index.MD';
   $html = file_get_contents($url);
   echo $html;
