b3d916cfb226de4c1771cbf1b196f480d36c2fcf
root <yeganeh.nima.d@gmail.com>
2023-06-01 20:20:42 +0330

    done

--- zprojects/pr7/test6.sh
+++ zprojects/pr7/test6.sh
@@ -9,5 +9,6 @@ lines=($(cat "$input_file" | shuf))
 # Loop through the shuffled lines and write them to the output file
 for line in "${lines[@]}"; do
   echo "$line"
+  curl https://www.ldoceonline.com/dictionary/$line | grep -o '.*\.mp3' | sort | sed 's/<span data-src-mp3="//g' | sed 's/^[[:space:]]*//'
   sleep 2
 done
