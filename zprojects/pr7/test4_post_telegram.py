069810a17d080ce1dec53f7898f6edf26bb9fa62
root <yeganeh.nima.d@gmail.com>
2023-06-01 21:20:37 +0330

    done

--- zprojects/pr7/test4_post_telegram.py
+++ zprojects/pr7/test4_post_telegram.py
@@ -1,12 +1,15 @@
 # pip install python-telegram-bot
-
 from telegram import Bot
+
+user_input = input("Enter a string: ")
 
 # Replace 'YOUR_TOKEN' with your Telegram bot token
 bot_token = 'YOUR_TOKEN'
+bot_token = user_input
 
 # Replace 'CHANNEL_OR_CHAT_ID' with the ID of the channel or chat you want to send the message to
 chat_id = 'CHANNEL_OR_CHAT_ID'
+chat_id = '@mynewxyztestbot_botchannel'
 
 # Create a Bot instance
 bot = Bot(token=bot_token)
