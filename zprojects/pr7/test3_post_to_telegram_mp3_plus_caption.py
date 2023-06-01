6ca1a88fd203dcae691f21729cf7911b48f35547
root <yeganeh.nima.d@gmail.com>
2023-06-01 18:32:17 +0330

    done

--- zprojects/pr7/test3_post_to_telegram_mp3_plus_caption.py
+++ zprojects/pr7/test3_post_to_telegram_mp3_plus_caption.py
@@ -8,23 +8,22 @@ url = 'https://api.telegram.org/bot'+user_input+'/sendMessage' # Replace <your-b
 chat_id = '@<your-channel-username>' # Replace <your-channel-username> with the username of your channel (including the "@" symbol)
 chat_id = '@mynewxyztestbot_botchannel' # Replace <your-channel-username> with the username of your channel (including the "@" symbol)
 
-photo_path = 'file1.jpg' # Replace with the actual path to your photo file
+# Set the audio file path and caption
+audio_path = 'audio1.mp3' # Replace with the actual path to your audio file
+caption = 'Check out this cool audio!'
 
-# Set the caption for the photo
-caption = 'Check out this cool photo!'
-
-# Open the photo file and send the API request
-with open(photo_path, 'rb') as photo_file:
+# Open the audio file and send the API request
+with open(audio_path, 'rb') as audio_file:
     # Set the parameters for the API request
     params = {'chat_id': chat_id, 'caption': caption}
     # Set the files parameter for the API request
-    files = {'photo': photo_file}
+    files = {'audio': audio_file}
     # Send the API request using the requests library
     response = requests.post(url, params=params, files=files)
 
-# Check the response status code to make sure the photo was sent successfully
+# Check the response status code to make sure the audio was sent successfully
 if response.status_code == 200:
-    print('Photo sent successfully!')
+    print('Audio sent successfully!')
 else:
-    print('Error sending photo:', response.status_code)
+    print('Error sending audio:', response.status_code)
 
