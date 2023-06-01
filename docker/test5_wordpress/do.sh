d9e24048d641ac116e638643b10578f7b8537287
root <yeganeh.nima.d@gmail.com>
2023-06-01 11:13:35 +0330

    done

---
+++ docker/test5_wordpress/do.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+for i in {1..5}
+do
+  echo $i
+  python3 test4_post_plus_image_upload.py
+done
---
+++ docker/test5_wordpress/requirement_install.sh
@@ -0,0 +1,3 @@
+sudo apt install python3-pip -y
+pip install python-wordpress-xmlrpc
+pip install openai
