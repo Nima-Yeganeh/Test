515b8268c087e202a7f182fa90ce390167c45624
root <yeganeh.nima.d@gmail.com>
2023-06-02 16:20:57 +0330

    done

--- zprojects/pr8/test15_main_by_test_tel_secret_file_and_input.py
+++ zprojects/pr8/test15_main_by_test_tel_secret_file_and_input.py
@@ -2,5 +2,11 @@ import test10_my_encrypt_decrypt_module
 
 # Call the function from my_module
 code = input("Enter a 4-character code: ")
-val1 = test10_my_encrypt_decrypt_module.my_function(code, secret_file)
 
+# bot_token = 'YOUR_TOKEN'
+bot_token = test10_my_encrypt_decrypt_module.my_function(code, "tel_bot_token.txt")
+print(bot_token)
+
+# chat_id = 'CHANNEL_OR_CHAT_ID'
+chat_id = test10_my_encrypt_decrypt_module.my_function(code, "tel_chat_id.txt")
+print(chat_id)
