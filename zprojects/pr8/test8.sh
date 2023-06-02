5b89ce618d5f4ae216053245218cd1fda5bfda99
root <yeganeh.nima.d@gmail.com>
2023-06-02 19:55:48 +0330

    done

--- zprojects/pr8/test8.sh
+++ zprojects/pr8/test8.sh
@@ -4,10 +4,10 @@ file1="file.txt"
 file2="filedone.txt"
 zmp3file="zmp3file.txt"
 zmp4file="zmp4file.txt"
+zjpgfile="zjpgfile.txt"
 echo "" > $file2
 
 while IFS= read -r line; do
-
   if ! grep -qF "$line" "$file2"; then
 
     # echo "$line"
@@ -15,52 +15,60 @@ while IFS= read -r line; do
 
     cat test8.txt | grep -o '.*\.mp3' | grep -oP 'href="\K[^"]+' | grep mp3 > test8_temp.txt
     # cat test8_temp.txt
-
     if [ -s "test8_temp.txt" ]; then
-
       echo "File is not empty."
       rm -f $zmp3file
-
       while IFS= read -r line; do
-
         # echo "$line"
-	url="$line"
-	filename=$(basename "$url" | sed 's/%28www.Next1.ir%29//')
-	# echo "$filename"
-	rm -f $filename
-	wget -O $filename $url
-	echo $filename >> $zmp3file
-
+        url="$line"
+        filename=$(basename "$url" | sed 's/%28www.Next1.ir%29//')
+        # echo "$filename"
+        rm -f $filename
+        wget -O $filename $url
+        echo $filename >> $zmp3file
       done < "test8_temp.txt"
       cat $zmp3file
       ls -anp | grep mp3
-
     else
       echo "File is empty."
     fi
 
     cat test8.txt | grep -o '.*\.mp4' | grep -oP 'href="\K[^"]+' | grep mp4 > test8_temp.txt
     # cat test8_temp.txt
-
     if [ -s "test8_temp.txt" ]; then
-
       echo "File is not empty."
       rm -f $zmp4file
-
       while IFS= read -r line; do
-
         # echo "$line"
-	url="$line"
-	filename=$(basename "$url" | sed 's/%28www.Next1.ir%29//')
-	# echo "$filename"
-	rm -f $filename
-	wget -O $filename $url
-	echo $filename >> $zmp4file
-
+        url="$line"
+        filename=$(basename "$url" | sed 's/%28www.Next1.ir%29//')
+        # echo "$filename"
+        rm -f $filename
+        wget -O $filename $url
+        echo $filename >> $zmp4file
       done < "test8_temp.txt"
       cat $zmp4file
       ls -anp | grep mp4
+    else
+      echo "File is empty."
+    fi
 
+    cat test8.txt | grep -o '.*\.jpg' | grep -oP 'src="\K[^"]+' | grep 480 > test8_temp.txt
+    # cat test8_temp.txt
+    if [ -s "test8_temp.txt" ]; then
+      echo "File is not empty."
+      rm -f $zjpgfile
+      while IFS= read -r line; do
+        # echo "$line"
+        url="$line"
+        filename=$(basename "$url" | sed 's/%28www.Next1.ir%29//')
+        # echo "$filename"
+        rm -f $filename
+        wget -O $filename $url
+        echo $filename >> $zjpgfile
+      done < "test8_temp.txt"
+      cat $zjpgfile
+      ls -anp | grep jpg
     else
       echo "File is empty."
     fi
@@ -69,9 +77,4 @@ while IFS= read -r line; do
     sleep 20
 
   fi
-
-  cat test8.txt | grep -o '.*\.jpg' | grep -oP 'src="\K[^"]+' | grep 480 > test8_temp.txt
-
-
 done < "$file1"
-
