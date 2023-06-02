cfce75c083d4422d33743a5d3aceb81b7d9d4af1
nima-yeganeh <yeganeh.nima.d@gmail.com>
2023-06-02 12:48:12 +0000

    done

--- zprojects/pr8/test7_post_telegram.py
+++ zprojects/pr8/test7_post_telegram.py
@@ -6,10 +6,13 @@ import test10_my_encrypt_decrypt_module
 code = input("Enter a 4-character code: ")
 user_input = input("Enter a string: ")
 
-bot_token = 'YOUR_TOKEN'
-bot_token = test10_my_encrypt_decrypt_module.my_function(code, 'tel_bot_token.txt')
-chat_id = 'CHANNEL_OR_CHAT_ID'
-chat_id = bot_token = test10_my_encrypt_decrypt_module.my_function(code, 'tel_chat_id.txt')
+# bot_token = 'YOUR_TOKEN'
+bot_token = test10_my_encrypt_decrypt_module.my_function(code, "tel_bot_token.txt")
+print(bot_token)
+
+# chat_id = 'CHANNEL_OR_CHAT_ID'
+chat_id = test10_my_encrypt_decrypt_module.my_function(code, "tel_chat_id.txt")
+print(chat_id)
 
 bot = Bot(token=bot_token)
 message = "Hello, Telegram!"
@@ -18,3 +21,4 @@ async def send_telegram_message():
     await bot.send_message(chat_id=chat_id, text=message)
 loop = asyncio.get_event_loop()
 loop.run_until_complete(send_telegram_message())
+
