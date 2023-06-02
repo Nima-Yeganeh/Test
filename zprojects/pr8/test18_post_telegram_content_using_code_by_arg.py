763249e3048bfcd49cc4cae35ec3976708ebaf7c
nima-yeganeh <yeganeh.nima.d@gmail.com>
2023-06-02 19:21:23 +0000

    done

--- zprojects/pr8/test18_post_telegram_content_using_code_by_arg.py
+++ zprojects/pr8/test18_post_telegram_content_using_code_by_arg.py
@@ -15,17 +15,18 @@ if len(sys.argv) > 1:
         content = file.readlines()
     if content:
         photo_path = content[0].strip()
-        # print(photo_path)
+        print(photo_path)
     else:
         print("File is empty.")
     captionfile = 'zcontentfile.txt'
     caption = ''
     with open(captionfile, 'r') as file:
         caption = file.read()
-    # print(caption)
+    print(caption)
+    photo_path = 'Rastaak-BadeMan-480x480.jpg'
     async def send_telegram_photo():
         with open(photo_path, 'rb') as image_file:
-            await bot.send_photo(chat_id=channel_id, photo=InputFile(image_file), caption=caption)
+            await bot.send_photo(chat_id=chat_id, photo=InputFile(image_file), caption=caption)
     loop = asyncio.get_event_loop()
     loop.run_until_complete(send_telegram_photo())
 else:
