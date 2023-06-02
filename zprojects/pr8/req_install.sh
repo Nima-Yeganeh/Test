57e86a1ba2ae01cec6921c86621c30e711244d43
root <yeganeh.nima.d@gmail.com>
2023-06-02 23:58:14 +0330

    done

--- zprojects/pr8/req_install.sh
+++ zprojects/pr8/req_install.sh
@@ -1,3 +1,4 @@
 pip install cryptography
 pip install python-telegram-bot
+pip install aiogram
 sudo apt-get install id3v2 -y
--- zprojects/pr8/test18_post_telegram_content_using_code_by_arg.py
+++ zprojects/pr8/test18_post_telegram_content_using_code_by_arg.py
@@ -49,6 +49,21 @@ if len(sys.argv) > 1:
             loop.run_until_complete(send_telegram_audio())
         else:
             print("File not found: ", mp3file_path)
+    mp4file = 'zmp4file.txt'
+    mp4file_path = ''
+    with open(mp4file, 'r') as file:
+        lines = file.readlines()
+    for line in lines:
+        mp4file_path = line.strip()
+        if os.path.isfile(mp4file_path) and os.path.getsize(mp4file_path) > 0:
+            print("File exists and has size greater than zero:", mp4file_path)
+            async def send_telegram_video():
+                with open(mp4file_path, 'rb') as video_file:
+                    await bot.send_video(chat_id=chat_id, video=InputFile(video_file), caption=caption)
+            loop = asyncio.get_event_loop()
+            loop.run_until_complete(send_telegram_video())
+        else:
+            print("File not found: ", mp4file_path)
 else:
     print("No input provided.")
 
