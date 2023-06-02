6a9dc27ffa3d18a027339d74b6068c0ac20ae954
nima-yeganeh <yeganeh.nima.d@gmail.com>
2023-06-02 12:36:13 +0000

    done

---
+++ zprojects/pr8/test14_main_by_input_code_secret_file.py
@@ -0,0 +1,7 @@
+import test10_my_encrypt_decrypt_module
+
+# Call the function from my_module
+code = input("Enter a 4-character code: ")
+secret_file = input("Enter your secret filename: ")
+test10_my_encrypt_decrypt_module.my_function(code, secret_file)
+
