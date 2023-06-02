1f49f6841a8b4c3a5c6bfbb683e5fadf3b5c229a
root <yeganeh.nima.d@gmail.com>
2023-06-02 16:11:42 +0330

    done

--- zprojects/pr8/test7_post_telegram.py
+++ zprojects/pr8/test7_post_telegram.py
@@ -1,29 +1,20 @@
 # pip install python-telegram-bot
 import asyncio
 from telegram import Bot
+import test10_my_encrypt_decrypt_module
 
+code = input("Enter a 4-character code: ")
 user_input = input("Enter a string: ")
 
-# Replace 'YOUR_TOKEN' with your Telegram bot token
 bot_token = 'YOUR_TOKEN'
-bot_token = user_input
-
-# Replace 'CHANNEL_OR_CHAT_ID' with the ID of the channel or chat you want to send the message to
+bot_token = test10_my_encrypt_decrypt_module.my_function(code, 'tel_bot_token.txt')
 chat_id = 'CHANNEL_OR_CHAT_ID'
-chat_id = '@mynewxyztestbot_botchannel'
+chat_id = bot_token = test10_my_encrypt_decrypt_module.my_function(code, 'tel_chat_id.txt')
 
-# Create a Bot instance
 bot = Bot(token=bot_token)
-
-# Define the message you want to send
 message = "Hello, Telegram!"
 
 async def send_telegram_message():
-    # Send the message
     await bot.send_message(chat_id=chat_id, text=message)
-
-# Create an asynchronous event loop
 loop = asyncio.get_event_loop()
-
-# Run the coroutine within the event loop
 loop.run_until_complete(send_telegram_message())
