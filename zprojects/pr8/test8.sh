9121c7bcec94480acd0484eb27a3d85ad91dc142
root <yeganeh.nima.d@gmail.com>
2023-06-02 18:27:34 +0330

    done

--- zprojects/pr8/test8.sh
+++ zprojects/pr8/test8.sh
@@ -7,6 +7,11 @@ file2="filedone.txt"
 while IFS= read -r line; do
   if ! grep -qF "$line" "$file2"; then
     echo "$line"
+    wget -O test8.txt "$line"
+    cat test8.txt | grep -o '.*\.mp3' | grep -oP 'href="\K[^"]+' > test8_temp.txt
+    cat test8_temp.txt
     echo "$line" >> $file2
+    sleep 10
   fi
 done < "$file1"
+
