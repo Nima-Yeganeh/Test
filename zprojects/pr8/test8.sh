bd6c312cf56bff2a9488147d83f27554b635b3d4
nima-yeganeh <yeganeh.nima.d@gmail.com>
2023-06-02 14:49:43 +0000

    done

--- zprojects/pr8/test8.sh
+++ zprojects/pr8/test8.sh
@@ -2,9 +2,11 @@
 
 file1="file.txt"
 file2="filedone.txt"
+# echo "" > $file2
 
 while IFS= read -r line; do
   if ! grep -qF "$line" "$file2"; then
     echo "$line"
+    echo "$line" >> $file2
   fi
 done < "$file1"
