e6ca79e0ba254cc96ef55225ad320ae3a5eae4f7
root <yeganeh.nima.d@gmail.com>
2023-06-03 18:48:51 +0330

    done

--- zprojects/pr9/test1.sh
+++ zprojects/pr9/test1.sh
@@ -1,1 +1,4 @@
 wget -U "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36 Edge/16.16299" -O test1sh.txt "https://www.tiktok.com/foryou"
+# cat test1sh.txt | grep -o '"UrlList":\["[^"]*"' | sed 's/"UrlList":\["//' | sed 's/"$//'
+cat test1sh.txt | grep -o '"UrlList":\["[^"]*"' | sed 's/"UrlList":\["//' | sed 's/"$//' | sed 's/u002F//g' | sed 's/\\/\//g'
+
