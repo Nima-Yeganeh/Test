584e66f01ed620027568ff4bd47f19f703d233fa
root <yeganeh.nima.d@gmail.com>
2023-06-03 17:15:48 +0330

    done

--- zprojects/pr9/test4.js
+++ zprojects/pr9/test4.js
@@ -1,6 +1,6 @@
 const TikTokScraper = require('tiktok-scraper');
 
-const url = 'https://www.tiktok.com/@test/video/71581153224555456456';
+const url = 'https://www.tiktok.com/@actorchristiankeyes/video/7230559113724366122';
 
 (async () => {
   try {
