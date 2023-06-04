1c09e259dbe4729259ab2a5d94b7fb25682750a3
root <yeganeh.nima.d@gmail.com>
2023-06-04 10:22:24 +0330

    done

---
+++ docker/test5_wordpress/reload.sh
@@ -0,0 +1,6 @@
+docker compose down
+docker volume rm test5_wordpress_db1_data
+docker volume rm test5_wordpress_redis1_data
+docker volume rm test5_wordpress_wordpress1_content
+docker compose up -d
+docker ps -a
