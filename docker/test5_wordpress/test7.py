1903ec5fef6cb66da3d79126c0c5b82d35972323
root <yeganeh.nima.d@gmail.com>
2023-06-04 10:49:46 +0330

    done

---
+++ docker/test5_wordpress/test7.py
@@ -0,0 +1,28 @@
+from wordpress_xmlrpc import Client, WordPressPost
+from wordpress_xmlrpc.methods import media, posts
+
+# Set up WordPress XML-RPC client
+url = 'http://test2.nimayeganeh.ir/xmlrpc.php'  # Replace with your WordPress site URL
+username = 'admin'  # Replace with your WordPress username
+password = 'P@ssw0rd'  # Replace with your WordPress password
+
+client = Client(url, username, password)
+
+# Path to the MP4 video file
+video_file_path = 'file.mp4'
+
+# Upload the video file to WordPress media library
+with open(video_file_path, 'rb') as video_file:
+    response = client.call(media.UploadFile(video_file.name, video_file.read()))
+
+# Get the URL of the uploaded video file
+video_url = response['url']
+
+# Create a new WordPress post with the video file
+post = WordPressPost()
+post.title = 'My Video Post'
+post.content = f'<video src="{video_url}" controls></video>'
+post.post_status = 'publish'
+
+# Publish the post
+client.call(posts.NewPost(post))
