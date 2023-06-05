b191cc6eccf326cce20bc1b559d8b9de4ffab768
root <yeganeh.nima.d@gmail.com>
2023-06-05 11:28:48 +0330

    done

--- docker/test5_wordpress/tshoot_commands.sh
+++ docker/test5_wordpress/tshoot_commands.sh
@@ -7,3 +7,13 @@ docker exec -it test5_wordpress-wordpress1-1 bash -c "wp redis status --allow-ro
 docker exec -it test5_wordpress-wordpress1-1 bash -c "wp plugin deactivate login-lockdown --allow-root"
 docker exec -it test5_wordpress-wordpress1-1 bash -c "wp plugin list --allow-root"
 docker exec -it test5_wordpress-wordpress1-1 bash -c "wp plugin install login-lockdown --activate --allow-root"
+
+docker exec -it test5_wordpress-wordpress1-1 bash -c "wp language core install ar --allow-root"
+docker exec -it test5_wordpress-wordpress1-1 bash -c "wp site switch-language ar --allow-root"
+
+docker exec -it test5_wordpress-wordpress1-1 bash -c "wp language core install en_US --allow-root"
+docker exec -it test5_wordpress-wordpress1-1 bash -c "wp site switch-language en_US --allow-root"
+
+docker exec -it test5_wordpress-wordpress1-1 bash -c "wp language core install fa_IR --allow-root"
+docker exec -it test5_wordpress-wordpress1-1 bash -c "wp site switch-language fa_IR --allow-root"
+
