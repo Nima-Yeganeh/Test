0b2be8c2ca62c0919e182b34fc0f16b2ff4a8a95
root <yeganeh.nima.d@gmail.com>
2023-06-06 19:39:26 +0330

    done

--- docker/test5_wordpress/src/test8.php
+++ docker/test5_wordpress/src/test8.php
@@ -33,20 +33,12 @@ if ($lineNumber !== false) {
                 $mp3_url = $line;
 		        $mp3_url = str_replace(' ', '', $mp3_url);
 		        $mp3_url = trim($mp3_url);
-                // Fetch the MP3 file contents
-                $mp3_data = file_get_contents($mp3_url);
-                // Check if the file data was retrieved successfully
-                if ($mp3_data === false) {
-                    echo "Failed to retrieve the MP3 file.";
-                    exit;
-                }
-                // Get the size of the MP3 file
-                $mp3_size = strlen($mp3_data);
-                // Set the appropriate content type and content length headers for playing audio
-                header('Content-type: audio/mpeg');
-                header("Content-length: $mp3_size");
-                // Output the MP3 file data
-                echo $mp3_data;
+                // Set appropriate headers for streaming
+                header('Content-Type: audio/mpeg');
+                header('Content-Length: ' . filesize($mp3_url));
+                header('Content-Disposition: inline; filename="file.mp3"');
+                // Stream the file
+                readfile($mp3_url);
                 break;
             }
         }
