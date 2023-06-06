1a17d470a575af4acbcd549e98c0254ffd4b500b
root <yeganeh.nima.d@gmail.com>
2023-06-06 18:07:24 +0330

    done

--- docker/test5_wordpress/src/test5.php
+++ docker/test5_wordpress/src/test5.php
@@ -1,10 +1,11 @@
 <?php
 
 // Get the URL entered by the user (assuming it's passed as a query parameter named "url")
-$url = isset($_GET['url']) ? $_GET['url'] : '';
+# $url = isset($_GET['url']) ? $_GET['url'] : '';
 
 // Extract the filename from the URL
-$filename = basename($url);
+# $filename = basename($url);
+$filename = $_GET['filename'];
 
 // Search for the filename in file1.txt and retrieve the line number
 $file = 'file1.txt';
