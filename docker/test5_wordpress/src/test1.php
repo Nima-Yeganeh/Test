35367393f35e85c59157a117c3f94537a76d2235
root <yeganeh.nima.d@gmail.com>
2023-06-06 17:21:25 +0330

    done

---
+++ docker/test5_wordpress/src/test1.php
@@ -0,0 +1,29 @@
+<?php
+// Get the filename from the URL query parameter
+$filename = $_GET['filename'];
+
+// Define the path to the directory where the video files are stored
+$videoDir = '';
+
+// Specify the full path of the video file
+$videoPath = $videoDir . $filename;
+
+// Check if the file exists
+if (file_exists($videoPath)) {
+    // Set the appropriate headers for video streaming
+    header('Content-Type: video/mp4');
+    header('Content-Length: ' . filesize($videoPath));
+
+    // Open the video file for reading
+    $file = fopen($videoPath, 'rb');
+
+    // Stream the video file content to the output buffer
+    fpassthru($file);
+
+    // Close the file
+    fclose($file);
+} else {
+    // File not found, handle the error accordingly
+    echo 'Video file not found.';
+}
+?>
---
+++ docker/test5_wordpress/src/test2.php
@@ -0,0 +1,8 @@
+<?php
+// Get the filename from the URL query parameter
+$filename = $_GET['filename'];
+// Print the filename
+echo $filename;
+// To test it >> enter http://domain.local/test2.php?filename=test.mp4
+?>
+
