741550675ab992d9c35197ddc081b8d5715e433d
root <yeganeh.nima.d@gmail.com>
2023-06-07 23:37:49 +0330

    done

--- zprojects/pr11/test8.sh
+++ zprojects/pr11/test8.sh
@@ -89,6 +89,7 @@ while IFS= read -r zline; do
       bash test9.sh | grep -E -e 'text-align: center;' -e '<br />' -e '</p>' | grep -vE '<a\s|http' | grep -v '<span id="more' | sed 's/<p style=.*center;">//g' | sed 's/<br \/>//g' | sed 's/<\/p>//g' | sed 's/<span.*;"> //g' | sed 's/<span.*;">//g' | sed 's/<\/span>//g' | sed 's/<strong>//g' | sed 's|</strong>||g' | grep -vE '<a\s|aligncenter' | sed 's/<p.*">//g' | grep -vE '<a\s|♫' | sed 's/<div.*">//g' | sed 's/<p>//g' >> $zdetailfile
 
 
+
       echo "ztitlefile:"
       cat $ztitlefile
       cat $ztitlefile > $zcontentfile
