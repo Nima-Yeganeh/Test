32b8797f379c08a7ce8a20366370fab97e9aed4b
root <yeganeh.nima.d@gmail.com>
2023-06-07 17:18:41 +0330

    done

--- zprojects/pr11/test8.sh
+++ zprojects/pr11/test8.sh
@@ -11,7 +11,10 @@ zengfile="zengfile.txt"
 zinfofile="zinfofile.txt"
 zdetailfile="zdetailfile.txt"
 zcontentfile="zcontentfile.txt"
-echo "" > $file2
+ztempfile="test8.txt"
+rm -f $file2
+touch $file2
+# echo "" > $file2
 
 # echo "Enter a 4-character code: "
 # read code
@@ -19,10 +22,18 @@ echo "" > $file2
 while IFS= read -r zline; do
   if ! grep -qF "$zline" "$file2"; then
 
-    # echo "$line"
-    wget -O test8.txt "$zline"
+    echo "$line"
+    rm -f $ztempfile
+    touch $ztempfile
+    # Loop until the file size is more than zero
+    while [ ! -s "$file_path" ]; do
+        echo "File size is zero, retrying..."
+        sleep 1
+    done
+    echo "File size is greater than zero."
+
+    # wget -O test8.txt "$zline"
 
   fi
 done < "$file1"
-
 
