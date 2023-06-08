0480eb72c047d19e880590d28a16d1a07995e719
root <yeganeh.nima.d@gmail.com>
2023-06-08 13:23:24 +0330

    done

---
+++ zprojects/pr11/test1.py
@@ -0,0 +1,8 @@
+zjpgfilename = "zjpgfilename.txt"
+
+# Read the file contents into a variable
+with open(zjpgfilename, 'r') as file:
+    file_contents = file.read()
+
+# Print the variable
+print(file_contents)
