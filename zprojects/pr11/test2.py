b377533b72206a0cf10e12b95d0fa838960e43a1
root <yeganeh.nima.d@gmail.com>
2023-06-08 17:09:11 +0330

    done

--- zprojects/pr11/test2.py
+++ zprojects/pr11/test2.py
@@ -6,17 +6,17 @@ file4_path = 'file4.htm'
 # Read contents of file1
 with open(file1_path, 'r') as file1:
     content1 = file1.read()
-with open(file4_path, 'a') as file2:
+with open(file4_path, 'a') as file4:
     file4.write(content1)
 
 # Read contents of file2
 with open(file2_path, 'r') as file1:
     content2 = file2.read()
-with open(file4_path, 'a') as file2:
+with open(file4_path, 'a') as file4:
     file4.write(content2)
 
 # Read contents of file3
 with open(file3_path, 'r') as file1:
     content3 = file3.read()
-with open(file4_path, 'a') as file2:
+with open(file4_path, 'a') as file4:
     file4.write(content3)
