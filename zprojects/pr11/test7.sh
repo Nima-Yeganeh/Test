86be2e31f6ff01dd55cb106a4f1617ac94fdebee
root <yeganeh.nima.d@gmail.com>
2023-06-08 22:35:55 +0330

    done

---
+++ zprojects/pr11/test7.sh
@@ -0,0 +1,27 @@
+zmp3fileurls="zmp3fileurl.txt"
+
+valid=true
+
+while IFS= read -r url; do
+  
+  echo $url
+  headers=$(curl -sI "$url")
+  content_length=$(echo "$headers" | grep -i Content-Length | awk '{print $2}' | tr -d '\r')
+
+  if [[ -n "$content_length" ]]; then
+    if (( content_length <= 1024 )); then
+      valid=false
+      break
+    fi
+  else
+    valid=false
+    break
+  fi
+
+done < "$zmp3fileurl"
+
+if $valid; then
+  echo "OK"
+else
+  echo "Not valid"
+fi
