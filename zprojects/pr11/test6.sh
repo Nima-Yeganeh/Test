b2772ef4cc1870ab56364b2558cdc5ff096da625
root <yeganeh.nima.d@gmail.com>
2023-06-08 21:50:07 +0330

    done

---
+++ zprojects/pr11/test6.sh
@@ -0,0 +1,14 @@
+url="https://dl.next1.ir/files/2015/04/tak/HoseinEstiri-DivoonatamDivoone-128%28www.Next1.ir%29.mp3"
+
+headers=$(curl -sI "$url")
+content_length=$(echo "$headers" | grep -i Content-Length | awk '{print $2}' | tr -d '\r')
+
+if [[ -n "$content_length" ]]; then
+  if (( content_length > 1024 )); then
+    echo "OK"
+  else
+    echo "Not valid"
+  fi
+else
+  echo "Failed to determine file size"
+fi
