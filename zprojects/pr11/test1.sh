16651111bbd2a7f65ea68236f3fc46c56a5ad792
nima-yeganeh <yeganeh.nima.d@gmail.com>
2023-06-08 11:40:17 +0000

    done

--- zprojects/pr11/test1.sh
+++ zprojects/pr11/test1.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 zmp3fileurl="zmp3fileurl.txt"  # Replace with the actual path to your file
+zmp3newfileurl="zmp3newfileurl.txt"
+
+rm -f $zmp3newfileurl
 
 # Read the file line by line and echo each line
 while IFS= read -r line; do
@@ -10,5 +13,7 @@ while IFS= read -r line; do
   filename=$(basename "$url" | sed 's/-320.*\.mp3/320.mp3/' | sed 's/-128.*\.mp3/128.mp3/')
   echo $filename
   echo $filename >> /root/test/docker/test5_wordpress/src/file1.txt
+  echo "https://test2-dl.nimayeganeh.ir/mp3-play.php?filename=$filename" >> $zmp3newfileurl
 done < "$zmp3fileurl"
+cat $zmp3newfileurl
 
