1baa451117b8e4fe87b35ddbb6eaaaa38efdc478
root <yeganeh.nima.d@gmail.com>
2023-06-09 16:40:47 +0330

    done

--- zprojects/pr11/test8.sh
+++ zprojects/pr11/test8.sh
@@ -223,7 +223,7 @@ while IFS= read -r zline; do
                 echo "$yline"
                 echo '<a href="'$yline'" download="file.mp3">Download MP3</a>' >> $zcontentfile
               done < "$zmp3newfileurl"
-
+              sleep 50
               # echo '<!DOCTYPE html><html><head></head><body><audio controls preload="auto" autoplay><source src="'$url'" type="audio/mpeg"></audio></body></html>' >> $zcontentfile
               # echo "" >> $zcontentfile
               echo "wordpress process..."
