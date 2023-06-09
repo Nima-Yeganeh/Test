b6a2314937f4e6e82638672da51de76f005095e0
nima-yeganeh <yeganeh.nima.d@gmail.com>
2023-06-09 14:04:44 +0000

    done

--- zprojects/pr11/test10.sh
+++ zprojects/pr11/test10.sh
@@ -1,6 +1,6 @@
               
 while IFS= read -r ylink; do
-  echo "$ylink"
+  echo "$ylink" | sed 's/mp3-play.php/mp3-download.php/g'
   ylinkfile=$(basename "$ylink" | sed 's/mp3-play.php?filename=//g')
   echo $ylinkfile
   echo '<a href="'$ylink'" download="'$ylinkfile'">Download MP3 File: '$ylinkfile'لینک دانلود فایل </a>'
