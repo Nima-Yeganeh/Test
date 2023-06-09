aa3b798038ce14ec728b300708fdde9633d00d98
nima-yeganeh <yeganeh.nima.d@gmail.com>
2023-06-09 13:41:14 +0000

    done

--- zprojects/pr11/test10.sh
+++ zprojects/pr11/test10.sh
@@ -1,7 +1,8 @@
-              echo "" >> $zcontentfile
-              while IFS= read -r ylink; do
-                echo "$ylink"
-                ylinkfile=$(basename "$ylink")
-                echo '<a href="'$ylink'" download="'$ylinkfile'">Download MP3 File: '$ylinkfile'لینک دانلود فایل </a>'
-              done < "$zmp3newfileurl"
+              
+while IFS= read -r ylink; do
+  echo "$ylink"
+  ylinkfile=$(basename "$ylink" | sed 's/mp3-play.php?filename=//g')
+  echo $ylinkfile
+  echo '<a href="'$ylink'" download="'$ylinkfile'">Download MP3 File: '$ylinkfile'لینک دانلود فایل </a>'
+done < "zmp3newfileurl.txt"
               
