1ba6dfbf0589751dd8149fce39d7b209abc8e222
root <yeganeh.nima.d@gmail.com>
2023-06-12 09:07:42 +0330

    done

--- zprojects/pr12/test14_decrypt_full.py
+++ zprojects/pr12/test14_decrypt_full.py
@@ -3,7 +3,13 @@ file_name = "secret.txt"
 with open(file_name, "r") as file:
     line1 = file.readline().rstrip('\n')
     line2 = file.readline().rstrip('\n')
-
+    line3 = file.readline().rstrip('\n')
+    line4 = file.readline().rstrip('\n')
+    line5 = file.readline().rstrip('\n')
+    
 print("Line 1:", line1)
 print("Line 2:", line2)
+print("Line 3:", line3)
+print("Line 4:", line4)
+print("Line 5:", line5)
 
