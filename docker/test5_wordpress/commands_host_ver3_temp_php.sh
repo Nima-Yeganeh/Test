c11d5845aafb29c67d53c8741235ae9c41174918
nima-yeganeh <yeganeh.nima.d@gmail.com>
2023-06-12 14:32:32 +0000

    done

---
+++ docker/test5_wordpress/commands_host_ver3_temp_php.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+newpath="/var/lib/docker/volumes/test5_wordpress_php1_data/_data/"
+echo $newpath
+ls -anp $newpath
+cp src/index.php $newpath
+cp src/index.htm $newpath
+cp src/index.html $newpath
+cp src/file1.txt $newpath
+cp src/file2.txt $newpath
+cp src/mp3-download.php $newpath
+cp src/mp3-play.php $newpath
+ls -anp $newpath
+
