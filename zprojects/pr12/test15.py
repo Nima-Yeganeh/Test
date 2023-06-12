c8035cfb05ac99d6b6ae95d11ed9c2e46d83ee15
root <yeganeh.nima.d@gmail.com>
2023-06-12 10:07:51 +0330

    done

--- zprojects/pr12/test15.py
+++ zprojects/pr12/test15.py
@@ -9,14 +9,15 @@ if len(sys.argv) == 3:
     # Print the arguments
     # print("Argument 1:", arg1)
     # print("Argument 2:", arg2)
-
+    
+    arg1 = int(sys.argv[1])
     if arg1 == 1:
         variable1 = "Value for variable 1"
         print(variable1)
     elif arg1 == 2:
         variable2 = "Value for variable 2"
         print(variable2)
-        
+
 else:
     # Print an error message if arguments are not provided
     print("Please provide two arguments.")
