0454909398bee5952c5ed775eeec4b5b4b52c7fd
root <yeganeh.nima.d@gmail.com>
2023-06-12 18:21:28 +0330

    done

--- zprojects/pr12/test1.sh
+++ zprojects/pr12/test1.sh
@@ -6,3 +6,7 @@ WP_Domain=$(python3 test16_decrypt_full_arg.py 3 $code)
 WP_User=$(python3 test16_decrypt_full_arg.py 4 $code)
 WP_Pass=$(python3 test16_decrypt_full_arg.py 5 $code)
 echo $Tel_Bot_Token
+echo $Tel_Chat_ID
+echo $WP_Domain
+echo $WP_User
+echo $WP_Pass
