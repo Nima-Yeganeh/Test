5d3eb647dc743a95612e050b1de3b2a44b4e8fa8
root <yeganeh.nima.d@gmail.com>
2023-06-12 18:19:56 +0330

    done

--- zprojects/pr12/test1.sh
+++ zprojects/pr12/test1.sh
@@ -1,6 +1,6 @@
 echo "Enter a 40-character code: "
 read code
-Tel_Bot_Token=$(python3 1 $code)
+Tel_Bot_Token=$(python3 test16_decrypt_full_arg.py 1 $code)
 Tel_Chat_ID=""
 WP_Domain=""
 WP_User=""
