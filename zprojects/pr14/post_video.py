13110e248e9171a9ecd21195973bdf7ed99cd0ea
root <yeganeh.nima.d@gmail.com>
2023-06-16 15:13:15 +0330

    done

--- zprojects/pr14/post_video.py
+++ zprojects/pr14/post_video.py
@@ -4,6 +4,6 @@ import userinfo
 client = Client()
 client.login(userinfo.username, userinfo.password)
 
-media = client.photo_upload('file.mp4', 'video!')
+media = client.video_upload('file.mp4', 'video!')
 
 client.logout()
