78edad8a00e065ecf8d816904381ded8c0b0b858
root <yeganeh.nima.d@gmail.com>
2023-06-16 13:18:21 +0330

    done

--- zprojects/pr14/test20.py
+++ zprojects/pr14/test20.py
@@ -5,6 +5,7 @@ xpass = input("Enter your pass: ")
 username = "hithisisatestacc"
 password = xpass
 video_path = "file.mp4"
+thumbnail_path = "photo1.jpg"
 
 client = Client()
 client.login(username, password)
@@ -18,7 +19,7 @@ caption = "Check out this awesome IGTV video!"
 # Upload the video to IGTV
 try:
     # Upload the video
-    igtv = client.igtv_upload(video_path, title=title, caption=caption)
+    igtv = client.igtv_upload(video_path, thumbnail=thumbnail_path, title=title, caption=caption)
 
     # Print the IGTV media ID
     print("IGTV video uploaded successfully. Media ID:", igtv.pk)
