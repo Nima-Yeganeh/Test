f8a05bf365392592e2653e974699dbfd354bf57b
root <yeganeh.nima.d@gmail.com>
2023-06-16 18:06:48 +0330

    done

--- zprojects/pr14/test.sh
+++ zprojects/pr14/test.sh
@@ -1,4 +1,4 @@
 rm -f temp.mp4
-url="https://snaptik.app/file.php?token=1NzIyMzgyNTU2OTI2NjI4OTkyMl92MTAwNDRnNTAwMDBjaDAydGxyYzc3dWE1cW1zNjRpMF9kaWFuYS1iZWxpdHNrYXk%3D2"
+url="https://snaptik.app/file.php?token=1NzIxMjI2NTAxNzk5Mzk1NjY1NF92MTIwNDRnZDAwMDBjZ2JobzlyYzc3dTBqaDU0Z3F2MF9zYW5qYXlh2"
 wget -O temp.mp4 "$url"
 python3 post_igtv.py
