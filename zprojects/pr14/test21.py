c89de11468de9428d5999ad0c3031bdc67441a63
root <yeganeh.nima.d@gmail.com>
2023-06-16 13:38:42 +0330

    done

---
+++ zprojects/pr14/test21.py
@@ -0,0 +1,20 @@
+import instagrapi
+from instagrapi import Client
+from instagrapi.types import MediaImage, MediaVideo
+
+xpass = input("Enter your pass: ")
+
+username = "hithisisatestacc"
+password = xpass
+video_path = "file.mp4"
+
+# Login to your Instagram account
+client = Client()
+client.login("your_username", "your_password")
+
+# Upload the video
+video = MediaVideo(video_path)
+media = client.post_video(video, caption="your_caption")
+
+# Logout
+client.logout()
