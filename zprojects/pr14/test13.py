b877bd6c2d6c36cb7e2a56171cde671707f8f663
root <yeganeh.nima.d@gmail.com>
2023-06-16 11:14:31 +0330

    done

---
+++ zprojects/pr14/test13.py
@@ -0,0 +1,21 @@
+from instabot import Bot
+
+xpass = input("Enter your pass: ")
+
+# Create an instance of the Bot class
+bot = Bot()
+
+# Log in to your Instagram account
+username = "hithisisatestacc"
+password = xpass
+bot.login(username=username, password=password)
+
+# Path to the video file
+video_path = "/root/temp/temp.mp4"
+
+# Upload the video to Instagram
+bot.upload_video(video_path, caption="Check out this awesome video!")
+
+# Log out from the Instagram account
+bot.logout()
+
