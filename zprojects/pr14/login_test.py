1a01c967f7838885da4dba71423c17c7d67639a3
root <yeganeh.nima.d@gmail.com>
2023-06-16 14:56:23 +0330

    done

--- zprojects/pr14/login_test.py
+++ zprojects/pr14/login_test.py
@@ -2,5 +2,5 @@ from instagrapi import Client
 import userinfo
 
 cl = Client()
-cl.login(userinfo.username, userinfo.password)
+cl.login(userinfo.username, userinfo.password, relogin='True')
 print(cl.phone_number)
