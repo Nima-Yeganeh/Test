09399f1ab880f1eeead5a3d879e60570ea704e63
root <yeganeh.nima.d@gmail.com>
2023-06-16 12:22:09 +0330

    done

--- zprojects/pr14/t2.py
+++ zprojects/pr14/t2.py
@@ -1,18 +1,15 @@
 import os
 
-# Function to extract the file extension
 def extract_file_extension(file_path):
     _, extension = os.path.splitext(file_path)
     return extension
 
-# Path to the file
 file_path = "file.mp4"
 
-# Extract the file extension
 extension = extract_file_extension(file_path)
+extension = extension.lstrip(".")
 
-# Print the file extension
 if extension:
-    print("File extension:", extension)
+    print(extension)
 else:
     print("Unable to determine file extension.")
