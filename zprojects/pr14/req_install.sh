011968e3660c28aac983859af4abef38105f4c35
root <yeganeh.nima.d@gmail.com>
2023-06-16 10:24:41 +0330

    done

--- zprojects/pr14/req_install.sh
+++ zprojects/pr14/req_install.sh
@@ -1,12 +1,12 @@
-python.exe -m pip install --upgrade pip
+# python.exe -m pip install --upgrade pip
 pip install selenium
-pip install instabot
-pip install instapy
-pip3 install selenium
-pip3 install instabot
-pip3 install instapy
-pip install emoji
-pip install --upgrade emoji
+# pip install instabot
+# pip install instapy
+# pip3 install selenium
+# pip3 install instabot
+# pip3 install instapy
+# pip install emoji
+# pip install --upgrade emoji
 # pip uninstall emoji
 # pip install emoji
 # pip install --upgrade emoji
--- zprojects/pr14/test9.py
+++ zprojects/pr14/test9.py
@@ -1,14 +1,15 @@
 # pip install instagrapi
-
 from instagrapi import Client
 from instagrapi.exceptions import ClientLoginRequired
 
+xpass = input("Enter your pass: ")
+
 # Set up your instagrapi session
 client = Client()
-client.login("user", "pass")
+client.login("hithisisatestacc", xpass)
 
 # Define the path to your image file and caption
-image_path = 'C:/Users/Nimay/nima_git/Test/insta/photo2.jpg'
+image_path = 'photo2.jpg'
 caption = "Lena was a busy mom who struggled to find time for healthy meals. She started cooking with vegetables and discovered how easy and delicious it could be. By incorporating vegetables into her meals, Lena found that she had more energy and felt fuller for longer. Vegetables are low in calories but high in fiber, vitamins, and minerals, which can help support a healthy immune system and reduce the risk of chronic diseases. Lena also discovered that cooking with vegetables could be a fun and creative way to try new recipes and experiment with different flavors and textures. She started making vegetable stir-fries, soups, and salads, and even replaced meat with veggies in some of her favorite dishes. Lena learned that by making small changes in her diet, she could make a big difference in her health and wellbeing. #drinks #food #restaurants #cooking #experience #likeforlike #instadaily"
 # caption = '#drinks #food #restaurants #cooking #experience #likeforlike #instadaily'
 
