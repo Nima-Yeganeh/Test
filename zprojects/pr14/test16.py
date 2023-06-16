e4ba34d42047aca31bafc96fe1bfbc1b15d91073
root <yeganeh.nima.d@gmail.com>
2023-06-16 11:39:23 +0330

    done

---
+++ zprojects/pr14/test16.py
@@ -0,0 +1,10 @@
+from instapy_cli import client
+
+xpass = input("Enter your pass: ")
+username = "hithisisatestacc"
+password = xpass
+video = '/root/temp/temp.mp4'
+text = 'This will be the caption of your video.' + '\r\n' + 'You can also use hashtags! #hash #tag #now'
+
+with client(username, password) as cli:
+    cli.upload(video, text)
