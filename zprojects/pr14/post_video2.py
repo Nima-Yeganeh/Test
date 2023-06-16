a4929f28c59556ec5e40d58fd021a7906032bb80
root <yeganeh.nima.d@gmail.com>
2023-06-16 15:54:10 +0330

    done

--- zprojects/pr14/post_video2.py
+++ zprojects/pr14/post_video2.py
@@ -4,6 +4,7 @@ import userinfo
 client = Client()
 client.login(userinfo.username, userinfo.password)
 
-media = client.video_upload('temp.mp4', 'new vid...!')
+# media = client.video_upload('temp.mp4', 'new vid...!')
+media = client.igtv_upload('temp.mp4', title='title!', caption='caption!')
 
 client.logout()
