d4712e8cca316acd25ac2b825f8dd66c37ef5798
root <yeganeh.nima.d@gmail.com>
2023-06-16 14:52:31 +0330

    done

--- zprojects/pr14/login_test.py
+++ zprojects/pr14/login_test.py
@@ -2,4 +2,4 @@ from instagrapi import Client
 import userinfo
 
 cl = Client()
-cl.login(userinfo.username, userinfo.password, relogin=ture)
+cl.login(userinfo.username, userinfo.password, relogin=Ture)
