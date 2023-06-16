9e8476dbfb6a0b3ea36a9febb59a07e14d52b768
root <yeganeh.nima.d@gmail.com>
2023-06-16 11:33:17 +0330

    done

--- zprojects/pr14/test15.py
+++ zprojects/pr14/test15.py
@@ -9,12 +9,14 @@ password = xpass
 # Path to the video file
 video_path = '/root/temp/temp.mp4'
 
+thumbnail_path = 'photo2.jpg'
+
 # Create an instance of the InstagramAPI class
 api = InstagramAPI(username, password)
 api.login()
 
 # Upload the video to Instagram
-api.uploadVideo(video_path, caption='Check out this awesome video!')
+api.uploadVideo(video_path, thumbnail=thumbnail_path, caption='Check out this awesome video!')
 
 # Logout from the Instagram account
 api.logout()
