1b3ea9c5ffb13dd3270e715321e0a1cd1f8441b9
root <yeganeh.nima.d@gmail.com>
2023-06-16 12:30:32 +0330

    done

---
+++ zprojects/pr14/test17.py
@@ -0,0 +1,10 @@
+from instapy_cli import client
+
+xpass = input("Enter your pass: ")
+username = "hithisisatestacc"
+password = xpass
+image = 'photo1.jpg'
+text = 'This will be the caption of your photo.' + '\r\n' + 'You can also use hashtags! #hash #tag #now'
+
+with client(username, password) as cli:
+    cli.upload(image, text)
