5e48695ee59562ae376f7dbbc7dca06c749988e6
root <yeganeh.nima.d@gmail.com>
2023-06-16 13:15:47 +0330

    done

---
+++ zprojects/pr14/test20.py
@@ -0,0 +1,28 @@
+from instagrapi import Client
+
+xpass = input("Enter your pass: ")
+
+username = "hithisisatestacc"
+password = xpass
+video_path = "file.mp4"
+
+client.login(username, password)
+
+# Title for the IGTV video
+title = "My IGTV Video"
+
+# Caption for the IGTV video (optional)
+caption = "Check out this awesome IGTV video!"
+
+# Upload the video to IGTV
+try:
+    # Upload the video
+    igtv = client.igtv_upload(video_path, title=title, caption=caption)
+
+    # Print the IGTV media ID
+    print("IGTV video uploaded successfully. Media ID:", igtv.pk)
+except Exception as e:
+    print("Error uploading IGTV video:", str(e))
+
+# Logout from Instagram
+client.logout()
