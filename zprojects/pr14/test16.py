ce3becb1dbc9c5b7a8bb45fcb74b819e6f68abb0
root <yeganeh.nima.d@gmail.com>
2023-06-16 11:40:32 +0330

    done

--- zprojects/pr14/test16.py
+++ zprojects/pr14/test16.py
@@ -3,7 +3,7 @@ from instapy_cli import client
 xpass = input("Enter your pass: ")
 username = "hithisisatestacc"
 password = xpass
-video = '/root/temp/temp.mp4'
+video = '/root/temp/file.mp4'
 text = 'This will be the caption of your video.' + '\r\n' + 'You can also use hashtags! #hash #tag #now'
 
 with client(username, password) as cli:
