e00d3fd43e8f14acc4f01e56b74a128bdd97afc5
root <yeganeh.nima.d@gmail.com>
2023-06-17 19:22:26 +0330

    done

--- zprojects/pr15/test.sh
+++ zprojects/pr15/test.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 file1="file.txt"
 file2="filedone.txt"
 
@@ -8,20 +7,22 @@ read code
 Tel_Bot_Token=$(python3 test_decrypt_full_arg.py 1 $code)
 Tel_Chat_ID=$(python3 test_decrypt_full_arg.py 2 $code)
 
-
-while IFS= read -r zline; do
-  if ! grep -qF "$zline" "$file2"; then
-    echo "**** Started ****"
-    echo "$zline"
-    rm -f temp.mp4
-    url=$zline
-    wget -O temp.mp4 "$url"
-    echo 'temp.mp4' > zmp4file.txt
-    python3 test18_post_telegram_content_using_code_by_arg.py $Tel_Bot_Token $Tel_Chat_ID
-    rm -f temp.mp4
-    echo "$zline" >> $file2
-    echo "**** Done! ****"
-    echo "**** Waiting for the next one ****"
-    sleep 20
-  fi
-done < "$file1"
+while true; do
+  while IFS= read -r zline; do
+    if ! grep -qF "$zline" "$file2"; then
+      echo "**** Started ****"
+      echo "$zline"
+      rm -f temp.mp4
+      url=$zline
+      wget -O temp.mp4 "$url"
+      echo 'temp.mp4' > zmp4file.txt
+      python3 test18_post_telegram_content_using_code_by_arg.py $Tel_Bot_Token $Tel_Chat_ID
+      rm -f temp.mp4
+      echo "$zline" >> $file2
+      echo "**** Done! ****"
+      echo "**** Waiting for the next one ****"
+      sleep 20
+    fi
+  done < "$file1"
+  git pull
+done
