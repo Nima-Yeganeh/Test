9c9c301d2ea537b143a6624be5e03686444c3e54
root <yeganeh.nima.d@gmail.com>
2023-06-17 09:34:09 +0330

    done

--- zprojects/pr14/post_igtv.py
+++ zprojects/pr14/post_igtv.py
@@ -1,8 +1,8 @@
 from instagrapi import Client
 xpass = input("Enter your pass: ")
-username = "hithisisatestacc"
+username = "viralvidforyou"
 password = xpass
 client = Client()
 client.login(username, password)
-media = client.igtv_upload('temp.mp4', title='newvid!', caption='#awesome #video #instagood #videooftheday #like4follow #short #happy #cute #followme #picoftheday #like4like #art #instadaily #friends #fun #funny #lol #humor #comedy #laugh #viral #entertainment')
+media = client.igtv_upload('temp.mp4', title='newvid!', caption='#explorepage #explore #awesome #video #instagood #videooftheday #like4follow #short #happy #cute #followme #picoftheday #like4like #art #instadaily #friends #fun #funny #lol #humor #comedy #laugh #viral #entertainment')
 client.logout()
--- zprojects/pr14/test.sh
+++ zprojects/pr14/test.sh
@@ -1,4 +1,4 @@
 rm -f temp.mp4
-url="https://snaptik.app/file.php?token=1NzIwMzU1OTYwODAzNDcyNTE2Nl92MTIwNDRnZDAwMDBjZnMydWZiYzc3dTUzZGZmY3M4Z19yaWNhcmRvLW5lbHNvbg%3D%3D2"
+url="https://snaptik.app/file.php?token=1NzIwNDY3MDc0NTc2MTM2OTM5MF92MTIwNDRnZDAwMDBjZnUyMzJyYzc3dWVyYWdjbTBoZ19nYW5hc2NvbmNhbmFz2"
 wget -O temp.mp4 "$url"
 python3 post_igtv.py
