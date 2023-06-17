d904deb079a62f5f10c9ffeb3eda8861edb1beae
root <yeganeh.nima.d@gmail.com>
2023-06-17 19:16:52 +0330

    done

--- zprojects/pr15/test.sh
+++ zprojects/pr15/test.sh
@@ -1,10 +1,27 @@
+#!/bin/bash
+
+file1="file.txt"
+file2="filedone.txt"
+
 echo "Enter a 40-character code: "
 read code
 Tel_Bot_Token=$(python3 test_decrypt_full_arg.py 1 $code)
 Tel_Chat_ID=$(python3 test_decrypt_full_arg.py 2 $code)
-rm -f temp.mp4
-url="https://snaptik.app/file.php?token=1NzIzNTQ3Mzg2Njk3MjIyMDY3OF92MDkwNDRnNDAwMDBjaGtwMTdiYzc3dWVhcnQzYzdlMF9hbmdlbGl0bw%3D%3D2"
-wget -O temp.mp4 "$url"
-echo 'temp.mp4' > zmp4file.txt
-python3 test18_post_telegram_content_using_code_by_arg.py $Tel_Bot_Token $Tel_Chat_ID
 
+
+while IFS= read -r zline; do
+  if ! grep -qF "$zline" "$file2"; then
+    echo "**** Started ****"
+    echo "$zline"
+    rm -f temp.mp4
+    url=$zline
+    wget -O temp.mp4 "$url"
+    echo 'temp.mp4' > zmp4file.txt
+    python3 test18_post_telegram_content_using_code_by_arg.py $Tel_Bot_Token $Tel_Chat_ID
+    rm -f temp.mp4
+    echo "$zline" >> $file2
+    echo "**** Done! ****"
+    echo "**** Waiting for the next one ****"
+    sleep 20
+  fi
+done < "$file1"
