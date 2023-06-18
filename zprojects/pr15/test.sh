caff7bd49992d5d62f232da64286a5de27671d49
root <yeganeh.nima.d@gmail.com>
2023-06-18 14:48:11 +0330

    done

--- zprojects/pr15/test.sh
+++ zprojects/pr15/test.sh
@@ -13,24 +13,19 @@ while true; do
       echo "**** Started ****"
       echo "$zline"
       rm -f downloads/*.mp4
-      # ls -anp downloads/
-      # rm -f temp.mp4
       url=$zline
       number=$(echo "$url" | grep -oE 'video/([0-9]+)' | cut -d '/' -f2)
       zfilename=$number'.mp4'
       echo $zfilename
       node index.js "$url"
-      # sleep 2
+      sleep 2
       file_path="downloads/$zfilename"
       echo $file_path
       if [ -s "$file_path" ]; then
           echo "OK >> $file_path"
-          # sleep 100
-          # wget -O temp.mp4 "$url"
-          # echo 'temp.mp4' > zmp4file.txt
           echo $file_path > zmp4file.txt
           python3 test18_post_telegram_content_using_code_by_arg.py $Tel_Bot_Token $Tel_Chat_ID
-          # rm -f temp.mp4
+          sleep 2
       else
           echo "File does not exist or is empty >> $file_path"
       fi
