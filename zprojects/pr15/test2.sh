80f6ca3b84fab4bc0e85bcd53a03411a6a8e0831
root <yeganeh.nima.d@gmail.com>
2023-06-20 15:13:47 +0330

    done

--- zprojects/pr15/test2.sh
+++
@@ -1,3 +0,0 @@
-url="https://www.tiktok.com/@lafamilia_lopez/video/7205261515639803182"
-number=$(echo "$url" | grep -oE '[0-9]+')
-echo "Number: $number"
--- zprojects/pr15/test3.sh
+++
@@ -1,3 +0,0 @@
-zfilename="1234.mp4"
-file_path="downloads/$zfilename"
-echo $file_path
