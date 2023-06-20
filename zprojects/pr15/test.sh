2771241b6d4678681c20de83d46d7bb65a3cb2f6
root <yeganeh.nima.d@gmail.com>
2023-06-20 15:00:22 +0330

    done

--- zprojects/pr15/test.sh
+++ zprojects/pr15/test.sh
@@ -6,6 +6,8 @@ echo "Enter a 40-character code: "
 read code
 Tel_Bot_Token=$(python3 test_decrypt_full_arg.py 1 $code)
 Tel_Chat_ID=$(python3 test_decrypt_full_arg.py 2 $code)
+Insta_ID=$(python3 test_decrypt_full_arg.py 6 $code)
+Insta_Pass=$(python3 test_decrypt_full_arg.py 7 $code)
 
 while true; do
   while IFS= read -r zline; do
@@ -24,7 +26,13 @@ while true; do
       if [ -s "$file_path" ]; then
           echo "OK >> $file_path"
           echo $file_path > zmp4file.txt
-          python3 test18_post_telegram_content_using_code_by_arg.py $Tel_Bot_Token $Tel_Chat_ID
+          echo "Telegram Post ..."
+          # python3 test18_post_telegram_content_using_code_by_arg.py $Tel_Bot_Token $Tel_Chat_ID
+          sleep 2
+          echo "Instagram Post ..."
+          python3 post_igtv.py $Insta_ID $Insta_Pass
+          echo "Insta Done!"
+          sleep 200
           sleep 2
       else
           echo "File does not exist or is empty >> $file_path"
