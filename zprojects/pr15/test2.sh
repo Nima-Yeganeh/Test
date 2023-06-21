3210ac51d950036c04022745296b3b4e87ce2d88
root <yeganeh.nima.d@gmail.com>
2023-06-21 12:10:45 +0330

    done

---
+++ zprojects/pr15/test2.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+file1="file.txt"
+file2="filedone.txt"
+
+while true; do
+  while IFS= read -r zline; do
+    if ! grep -qF "$zline" "$file2"; then
+      echo "**** Started ****"
+      echo "$zline"
+      break
+  done < "$file1"
+done
