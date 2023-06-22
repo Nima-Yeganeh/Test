e7bdc70bc86b344c8f649389def862620068265e
root <yeganeh.nima.d@gmail.com>
2023-06-22 19:46:47 +0330

    done

--- docker/test5_wordpress/test.sh
+++ docker/test5_wordpress/test.sh
@@ -1,2 +1,3 @@
 targethost="test5_wordpress-wordpress1-1"
-docker exec $targethost bash -c "wp post update 2 --post_content='New Contact' --allow-root"
+zcontact="New Contact 2"
+docker exec $targethost bash -c "wp post update 2 --post_content='$zcontact' --allow-root"
