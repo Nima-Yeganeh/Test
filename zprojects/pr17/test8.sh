094c609809c411fd320a2d9936f468781b572677
root <yeganeh.nima.d@gmail.com>
2023-06-24 20:45:11 +0330

    done

--- zprojects/pr17/test8.sh
+++ zprojects/pr17/test8.sh
@@ -27,7 +27,8 @@ while true; do
       # echo $title
       desc=$(cat test1.txt | grep 'description' | grep -oP '(?<=description": ")[^"]*' | sed 's/&quot;//g' | head -n1)
       # echo $desc
-      tags=$(cat test1.txt | grep 'og:video:tag' | grep -oP '(?<=<meta property="og:video:tag" content=")[^"]*' | sed 's/^[[:space:]]*//' | sed 's/[[:space:]]*$//' | sed 's/ /_/g' | sed 's/^[[:space:]]*/#/')
+      # tags=$(cat test1.txt | grep 'og:video:tag' | grep -oP '(?<=<meta property="og:video:tag" content=")[^"]*' | sed 's/^[[:space:]]*//' | sed 's/[[:space:]]*$//' | sed 's/ /_/g' | sed 's/^[[:space:]]*/#/')
+      tags=$(cat test1.txt | grep 'og:video:tag' | grep -oP '(?<=<meta property="og:video:tag" content=")[^"]*' | sed 's/^[[:space:]]*//' | sed 's/[[:space:]]*$//')
       # echo $tags
       vidurl=$(cat test1.txt | grep 'contentUrl' | grep -oP '(?<=contentUrl": ")[^"]*' | head -n1)
       echo $vidurl
