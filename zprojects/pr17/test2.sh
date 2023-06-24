7ad3b44e9259f0eb137d3d72c425373f1e9deaaa
root <yeganeh.nima.d@gmail.com>
2023-06-24 11:33:33 +0330

    done

--- zprojects/pr17/test2.sh
+++ zprojects/pr17/test2.sh
@@ -2,3 +2,5 @@ title=$(cat test1.txt | grep 'name' | grep -oP '(?<=name": ")[^"]*' | head -n1)
 echo $title
 desc=$(cat test1.txt | grep 'description' | grep -oP '(?<=description": ")[^"]*' | head -n1)
 echo $desc
+tags=$(cat test1.txt | grep 'og:video:tag' | grep -oP '(?<=<meta property="og:video:tag" content=")[^"]*' | sed 's/^[[:space:]]*//' | sed 's/^[[:space:]]*/#/')
+echo $tags
