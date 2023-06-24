79a5e436d293071111e0f90a8832828c0bbb8625
root <yeganeh.nima.d@gmail.com>
2023-06-24 11:26:16 +0330

    done

--- zprojects/pr17/test2.sh
+++ zprojects/pr17/test2.sh
@@ -1,2 +1,4 @@
 title=$(cat test1.txt | grep 'name' | grep -oP '(?<=name": ")[^"]*' | head -n1)
 echo $title
+desc=$(cat test1.txt | grep 'description' | grep -oP '(?<=description": ")[^"]*' | head -n1)
+echo $desc
