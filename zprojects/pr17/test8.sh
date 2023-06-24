18bc328c8de713ace4fc19afc0fe6a7f54d79be9
root <yeganeh.nima.d@gmail.com>
2023-06-24 21:41:04 +0330

    done

--- zprojects/pr17/test8.sh
+++ zprojects/pr17/test8.sh
@@ -13,6 +13,7 @@ while true; do
   rm -f logfile.txt
   url="https://video.varzesh3.com/"
   wget -O test3.txt "$url" > logfile.txt 2>&1
+  rm -f logfile.txt
   cat test3.txt | grep 'data-nt-link href' | grep 'title' | grep -oP '(?<=<a class="title" data-nt-link href=")[^"]*' | grep -oP '(?<=/video/)\d+' | uniq | sort -n | uniq | sort -n | sed 's/^/https:\/\/video.varzesh3.com\/video\//' > $file1
   cat $file1 | wc -l
   while IFS= read -r zline; do
