ce8b0730fe834788bdbb11dfc8db122c5cd9a73c
root <yeganeh.nima.d@gmail.com>
2023-06-24 16:38:34 +0330

    done

--- zprojects/pr17/test10.sh
+++ zprojects/pr17/test10.sh
@@ -1,4 +1,4 @@
-if [ $(stat -c%s "file.mp4") -lt 20000000 ]; then
+if [ $(stat -c%s "output.mp4") -lt 20000000 ]; then
     echo "File size is lower than 20 megabytes. OK"
 else
     echo "File size is larger than or equal to 20 megabytes. Not OK"
