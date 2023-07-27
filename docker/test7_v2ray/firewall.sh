1c8fa6d1e2a61a79ce3b9ba6fe0a8540c12981cb
root <yeganeh.nima.d@gmail.com>
2023-07-27 20:24:35 +0330

    done

--- docker/test7_v2ray/firewall.sh
+++ docker/test7_v2ray/firewall.sh
@@ -1,13 +1,15 @@
 sudo ufw allow 443/tcp
 sudo ufw allow 443/udp
-sudo ufw status
-sudo ufw reload
-sudo systemctl stop ufw
-sudo systemctl start ufw
-sudo systemctl restart ufw
-
 sudo ufw allow 3389/tcp
 sudo ufw allow 3389/udp
+sudo ufw allow 1010/tcp
+sudo ufw allow 1010/udp
+sudo ufw allow 1110/tcp
+sudo ufw allow 1110/udp
+sudo ufw allow 1310/tcp
+sudo ufw allow 1310/udp
+sudo ufw allow 1320/tcp
+sudo ufw allow 1320/udp
 sudo ufw status
 sudo ufw reload
 sudo systemctl stop ufw
